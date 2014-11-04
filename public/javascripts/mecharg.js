50d0dc181a923145f0ab6124b4c9cde09c2469b0
Kristian Lange <lange.kristian@gmail.com>
2014-11-04 19:40:32 +0100

    mecharg.js loads studyData and componentData automatically.

--- public/javascripts/mecharg.js
+++ public/javascripts/mecharg.js
@@ -5,101 +5,111 @@
  */
 
 var mecharg = {};
+var onErrorCallback;
+var onLoadCallback;
 
 window.addEventListener('load', onload);
 
-function onload() {
-	mecharg.readIdCookie();
+mecharg.onError = function(callback) {
+	onErrorCallback = callback;
 }
 
-/**
- * Reads MechArg's ID cookie and stores all key-value pairs into mecharg scope
- * This function is automatically called after the page is loaded, so it's not
- * necessary to call it again.
- */
-mecharg.readIdCookie = function() {
-	var nameEQ = escape("MechArg_IDs") + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) === ' ') {
-			c = c.substring(1, c.length);
-		}
-		if (c.indexOf(nameEQ) === 0) {
-			var cookieStr = unescape(c.substring(nameEQ.length + 1,
-					c.length - 1));
-			var idMap = cookieStr.split("&");
-			idMap.forEach(function(entry) {
-				var keyValuePair = entry.split("=");
-				mecharg[keyValuePair[0]] = keyValuePair[1];
-			});
+mecharg.onLoad = function(callback) {
+	onLoadCallback = callback;
+}
+
+function onload() {
+	var studyDataReady = false;
+	var componentDataReady = false;
+
+	ready = function() {
+		if (studyDataReady && componentDataReady) {
+			if (onLoadCallback) {
+				onLoadCallback();
+			}
 		}
 	}
-}
 
-/**
- * Gets the study's data from the MechArg server and stores them in
- * mecharg.studyData (the whole data) and mecharg.studyJsonData (just the JSON data).
- * 
- * @param {optional
- *            Function} success - Function to be called in case of success.
- * @param {optional
- *            Function} error - Function to be called in case server returns an
- *            error.
- */
-mecharg.getStudyData = function(success, error) {
-	$.ajax({
-		url : "/publix/" + mecharg.studyId + "/getData",
-		type : "GET",
-		dataType : 'json',
-		success : function(response) {
-			mecharg.studyData = response;
-			mecharg.studyJsonData = $.parseJSON(mecharg.studyData.jsonData);
-			if (success) {
-				success(response)
+	/**
+	 * Reads MechArg's ID cookie and stores all key-value pairs into mecharg
+	 * scope This function is automatically called after the page is loaded, so
+	 * it's not necessary to call it again.
+	 */
+	readIdCookie = function() {
+		var nameEQ = escape("MechArg_IDs") + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) === ' ') {
+				c = c.substring(1, c.length);
 			}
-			;
-		},
-		error : function(err) {
-			if (error) {
-				error(err.responseText)
+			if (c.indexOf(nameEQ) === 0) {
+				var cookieStr = unescape(c.substring(nameEQ.length + 1,
+						c.length - 1));
+				var idMap = cookieStr.split("&");
+				idMap.forEach(function(entry) {
+					var keyValuePair = entry.split("=");
+					mecharg[keyValuePair[0]] = keyValuePair[1];
+				});
 			}
-			;
 		}
-	});
-}
+	}
 
-/**
- * Gets the component's data from the MechArg server and stores them in
- * mecharg.componentData (the whole data) and mecharg.componentJsonData (just the JSON data).
- * 
- * @param {optional
- *            Function} success - Function to be called in case of success.
- * @param {optional
- *            Function} error - Function to be called in case server returns an
- *            error.
- */
-mecharg.getComponentData = function(success, error) {
-	$.ajax({
-		url : "/publix/" + mecharg.studyId + "/" + mecharg.componentId
-				+ "/getData",
-		type : "GET",
-		dataType : 'json',
-		success : function(response) {
-			mecharg.componentData = response;
-			mecharg.componentJsonData = $.parseJSON(mecharg.componentData.jsonData);
-			if (success) {
-				success(response)
+	/**
+	 * Gets the study's data from the MechArg server and stores them in
+	 * mecharg.studyData (the whole data) and mecharg.studyJsonData (just the
+	 * JSON data).
+	 */
+	getStudyData = function() {
+		$
+				.ajax({
+					url : "/publix/" + mecharg.studyId + "/getData",
+					type : "GET",
+					dataType : 'json',
+					success : function(response) {
+						mecharg.studyData = response;
+						mecharg.studyJsonData = $
+								.parseJSON(mecharg.studyData.jsonData);
+						studyDataReady = true;
+						ready();
+					},
+					error : function(err) {
+						if (onErrorCallback) {
+							onErrorCallback(err.responseText);
+						}
+					}
+				});
+	}
+
+	/**
+	 * Gets the component's data from the MechArg server and stores them in
+	 * mecharg.componentData (the whole data) and mecharg.componentJsonData
+	 * (just the JSON data).
+	 */
+	getComponentData = function() {
+		$.ajax({
+			url : "/publix/" + mecharg.studyId + "/" + mecharg.componentId
+					+ "/getData",
+			type : "GET",
+			dataType : 'json',
+			success : function(response) {
+				mecharg.componentData = response;
+				mecharg.componentJsonData = $
+						.parseJSON(mecharg.componentData.jsonData);
+				componentDataReady = true;
+				ready();
+			},
+			error : function(err) {
+				if (onErrorCallback) {
+					onErrorCallback(err.responseText);
+				}
 			}
-			;
-		},
-		error : function(err) {
-			if (error) {
-				error(err.responseText)
-			}
-			;
-		}
-	});
+		});
+	}
+
+	readIdCookie();
+	getStudyData();
+	getComponentData();
 }
 
 /**
@@ -110,31 +120,26 @@ mecharg.getComponentData = function(success, error) {
  * @param {optional
  *            Function} success - Function to be called in case of successful
  *            submit
- * @param {optional
- *            Function} error - Function to be called in case server returns an
- *            error
  */
-mecharg.submitResultData = function(resultData, success, error) {
+mecharg.submitResultData = function(resultData, success) {
 	var resultJson = JSON.stringify(resultData);
-	
+
 	$.ajax({
 		url : "/publix/" + mecharg.studyId + "/" + mecharg.componentId
 				+ "/submitResultData",
 		data : resultJson,
 		processData : false,
 		type : "POST",
-		contentType : "text/plain", //"application/json",
+		contentType : "application/json",
 		success : function(response) {
 			if (success) {
 				success(response)
 			}
-			;
 		},
 		error : function(err) {
-			if (error) {
-				error(err.responseText)
+			if (onErrorCallback) {
+				onErrorCallback(err.responseText);
 			}
-			;
 		}
 	});
 }
