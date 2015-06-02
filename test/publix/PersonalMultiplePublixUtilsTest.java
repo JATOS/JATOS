26439500aa4d29e7fd6d80afc82efe48f4eaa8d3
Kristian Lange <lange.kristian@gmail.com>
2015-06-02 20:11:36 +0200

    Added tests for personal multiple Publix

---
+++ test/publix/PersonalMultiplePublixUtilsTest.java
@@ -0,0 +1,108 @@
+package publix;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import models.StudyModel;
+import models.StudyResult.StudyState;
+import models.workers.PersonalMultipleWorker;
+
+import org.fest.assertions.Fail;
+import org.junit.Test;
+
+import common.Global;
+
+import controllers.publix.personal_multiple.PersonalMultipleErrorMessages;
+import controllers.publix.personal_multiple.PersonalMultiplePublixUtils;
+import exceptions.publix.ForbiddenPublixException;
+import exceptions.publix.PublixException;
+
+/**
+ * @author Kristian Lange
+ */
+public class PersonalMultiplePublixUtilsTest extends
+		PublixUtilsTest<PersonalMultipleWorker> {
+
+	private PersonalMultipleErrorMessages personalMultipleErrorMessages;
+	private PersonalMultiplePublixUtils personalMultiplePublixUtils;
+
+	@Override
+	public void before() throws Exception {
+		super.before();
+		personalMultiplePublixUtils = Global.INJECTOR
+				.getInstance(PersonalMultiplePublixUtils.class);
+		publixUtils = personalMultiplePublixUtils;
+		personalMultipleErrorMessages = Global.INJECTOR
+				.getInstance(PersonalMultipleErrorMessages.class);
+		errorMessages = personalMultipleErrorMessages;
+	}
+
+	@Override
+	public void after() throws Exception {
+		super.before();
+	}
+
+	@Test
+	public void checkRetrieveTypedWorker() throws NoSuchAlgorithmException,
+			IOException, PublixException {
+		PersonalMultipleWorker worker = new PersonalMultipleWorker();
+		addWorker(worker);
+
+		PersonalMultipleWorker retrievedWorker = publixUtils
+				.retrieveTypedWorker(worker.getId().toString());
+		assertThat(retrievedWorker.getId()).isEqualTo(worker.getId());
+	}
+
+	@Test
+	public void checkRetrieveTypedWorkerWrongType()
+			throws NoSuchAlgorithmException, IOException, PublixException {
+		try {
+			publixUtils.retrieveTypedWorker(admin.getWorker().getId()
+					.toString());
+			Fail.fail();
+		} catch (PublixException e) {
+			assertThat(e.getMessage()).isEqualTo(
+					personalMultipleErrorMessages.workerNotCorrectType(admin
+							.getWorker().getId()));
+		}
+	}
+
+	@Test
+	public void checkWorkerAllowedToDoStudy() throws NoSuchAlgorithmException,
+			IOException, ForbiddenPublixException {
+		StudyModel study = importExampleStudy();
+		study.addAllowedWorker(PersonalMultipleWorker.WORKER_TYPE);
+		addStudy(study);
+		PersonalMultipleWorker worker = new PersonalMultipleWorker();
+		addWorker(worker);
+
+		publixUtils.checkWorkerAllowedToDoStudy(worker, study);
+
+		// Clean-up
+		removeStudy(study);
+	}
+
+	@Test
+	public void checkWorkerAllowedToDoStudyFinishedStudy()
+			throws NoSuchAlgorithmException, IOException,
+			ForbiddenPublixException {
+		StudyModel study = importExampleStudy();
+		study.addAllowedWorker(PersonalMultipleWorker.WORKER_TYPE);
+		addStudy(study);
+		PersonalMultipleWorker worker = new PersonalMultipleWorker();
+		addWorker(worker);
+
+		// State of study has no influence. Personal multiple workers can do
+		// studies multiple times
+		addStudyResult(study, worker, StudyState.FINISHED);
+		publixUtils.checkWorkerAllowedToDoStudy(worker, study);
+
+		addStudyResult(study, worker, StudyState.FINISHED);
+
+		// Clean-up
+		removeStudy(study);
+	}
+
+}
