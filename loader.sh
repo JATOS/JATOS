955335efc8e1c934f9cb53a0ee034397bf946eb6
kristian-lange <lange.kristian@gmail.com>
2016-11-04 17:28:21 +0100

    Fix https://github.com/JATOS/JATOS/issues/136 allow spaces in Linux / Unix path

--- loader.sh
+++ loader.sh
@@ -10,7 +10,7 @@ port="9000"
 
 # Get JATOS directory
 dir="$( cd "$( dirname "$0" )" && pwd )"
-pidfile=$dir/RUNNING_PID
+pidfile="$dir/RUNNING_PID"
 
 function start() {
 	checkAlreadyRunning
@@ -21,42 +21,42 @@ function start() {
 	# Generate application secret for the Play framework
 	secret="$(LC_ALL=C tr -cd '[:alnum:]' < /dev/urandom | fold -w128 | head -n1)"
 
-	if [ ! -f $dir/bin/jatos ]
+	if [ ! -f "$dir/bin/jatos" ]
 	then
 		echo -e "\n$dir/bin/jatos doesn't exist!"
 		exit 1
 	fi
 	
 	# In case './bin/jatos' isn't executable set the x bit
-	chmod u+x $dir/bin/jatos
+	chmod u+x "$dir/bin/jatos"
 	
 	# Start JATOS with configuration file and application secret
-	$dir/bin/jatos -Dconfig.file="$dir/conf/production.conf" -Dplay.crypto.secret=$secret -Dhttp.port=$port -Dhttp.address=$address -J-server > /dev/null &
+	"$dir/bin/jatos" -Dconfig.file="$dir/conf/production.conf" -Dplay.crypto.secret=$secret -Dhttp.port=$port -Dhttp.address=$address -J-server > /dev/null &
 	
 	echo "...started"
 	echo "To use JATOS type $address:$port in your browser's address bar"
 }
 
 function stop() {
-	if [ ! -f $pidfile ] || ! kill -0 $(cat "$pidfile") 2>1 >/dev/null
+	if [ ! -f "$pidfile" ] || ! kill -0 $(cat "$pidfile") 2>1 >/dev/null
 	then
-		echo "JATOS appears not to be running. Maybe another user is running it?"
+		echo "JATOS not running"
 		exit 1
 	fi
 	echo -n "Stopping JATOS"
-	kill -SIGTERM $(cat $pidfile)
-	rm -f $pidfile
+	kill -SIGTERM $(cat "$pidfile")
+	rm -f "$pidfile"
 	echo "...stopped"
 }
 
 function checkAlreadyRunning() {
-	if [ -f $pidfile ] && kill -0 $(cat $pidfile) 2>1 >/dev/null
+	if [ -f "$pidfile" ] && kill -0 $(cat "$pidfile") 2>1 >/dev/null
 	then
 		echo "There seems to be a running JATOS."
 		exit 1
 	fi
 	# Delete old PID file (just to be sure)
-	rm -f $pidfile
+	rm -f "$pidfile"
 
 	if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null
 	then
@@ -80,11 +80,11 @@ function checkJava() {
 	# If we don't have a version or if the version is not a number or if the version is smaller 18 (Java 8) try to find a local Java installation
 	if [[ -z "$java_version" ]] || [[ ! "$java_version" =~ ^[0-9]+$ ]] || [[ "$java_version" -lt 18 ]]
 	then
-		if [[ -n $dir/jre/linux_x64_jre ]] && [[ -x "$dir/jre/linux_x64_jre/bin/java" ]]
+		if [[ -n "$dir/jre/linux_x64_jre" ]] && [[ -x "$dir/jre/linux_x64_jre/bin/java" ]]
 		then
 			echo "Jatos uses local JRE"
 			export JAVA_HOME="$dir/jre/linux_x64_jre"
-		elif [[ -n $dir/jre/mac_x64_jre ]] && [[ -x "$dir/jre/mac_x64_jre/bin/java" ]]
+		elif [[ -n "$dir/jre/mac_x64_jre" ]] && [[ -x "$dir/jre/mac_x64_jre/bin/java" ]]
 		then
 			echo "Jatos uses local JRE"
 			export JAVA_HOME="$dir/jre/mac_x64_jre"
