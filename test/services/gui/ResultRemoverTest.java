b8847fda1b9b0c7a9869a4cc01efc7e402003dae
kristian-lange <lange.kristian@gmail.com>
2017-02-14 20:11:52 +0100

    Fixed ResultRemoverTest after upgrade

--- test/services/gui/ResultRemoverTest.java
+++ test/services/gui/ResultRemoverTest.java
@@ -5,20 +5,35 @@ import static org.fest.assertions.Assertions.assertThat;
 import java.io.IOException;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.fest.assertions.Fail;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import daos.common.StudyDao;
+import daos.common.StudyResultDao;
+import daos.common.UserDao;
 import exceptions.gui.BadRequestException;
 import exceptions.gui.ForbiddenException;
 import exceptions.gui.NotFoundException;
 import exceptions.publix.ForbiddenReloadException;
-import general.AbstractTest;
+import general.TestHelper;
 import general.common.MessagesStrings;
 import models.common.ComponentResult;
 import models.common.Study;
 import models.common.StudyResult;
 import models.common.User;
-import services.gui.ResultRemover;
+import play.ApplicationLoader;
+import play.Environment;
+import play.db.jpa.JPAApi;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.guice.GuiceApplicationLoader;
+import services.publix.ResultCreator;
 import services.publix.workers.JatosPublixUtils;
 
 /**
@@ -26,65 +41,50 @@ import services.publix.workers.JatosPublixUtils;
  * 
  * @author Kristian Lange
  */
-public class ResultRemoverTest extends AbstractTest {
+public class ResultRemoverTest {
 
+	private Injector injector;
+
+	@Inject
+	private TestHelper testHelper;
+
+	@Inject
+	private JPAApi jpaApi;
+
+	@Inject
 	private ResultRemover resultRemover;
+
+	@Inject
+	private ResultCreator resultCreator;
+
+	@Inject
+	private ResultService resultService;
+
+	@Inject
 	private JatosPublixUtils jatosPublixUtils;
 
-	@Override
-	public void before() throws Exception {
-		resultRemover = application.injector().instanceOf(ResultRemover.class);
-		jatosPublixUtils = application.injector()
-				.instanceOf(JatosPublixUtils.class);
+	@Inject
+	private UserDao userDao;
+
+	@Inject
+	private StudyDao studyDao;
+
+	@Inject
+	private StudyResultDao studyResultDao;
+
+	@Before
+	public void startApp() throws Exception {
+		GuiceApplicationBuilder builder = new GuiceApplicationLoader()
+				.builder(new ApplicationLoader.Context(Environment.simple()));
+		injector = Guice.createInjector(builder.applicationModule());
+		injector.injectMembers(this);
 	}
 
-	@Override
-	public void after() throws Exception {
-	}
-
-	private void createTwoStudyResults(Study study)
-			throws ForbiddenReloadException {
-		entityManager.getTransaction().begin();
-		StudyResult studyResult1 = resultCreator.createStudyResult(study,
-				study.getDefaultBatch(), admin.getWorker());
-		// Have to set worker manually in test - don't know why
-		studyResult1.setWorker(admin.getWorker());
-		ComponentResult componentResult11 = jatosPublixUtils
-				.startComponent(study.getFirstComponent(), studyResult1);
-		componentResult11.setData(
-				"First ComponentResult's data of the first StudyResult.");
-		ComponentResult componentResult12 = jatosPublixUtils
-				.startComponent(study.getFirstComponent(), studyResult1);
-		componentResult12.setData(
-				"Second ComponentResult's data of the first StudyResult.");
-
-		StudyResult studyResult2 = resultCreator.createStudyResult(study,
-				study.getBatchList().get(0), admin.getWorker());
-		// Have to set worker manually in test - don't know why
-		studyResult2.setWorker(admin.getWorker());
-		ComponentResult componentResult21 = jatosPublixUtils
-				.startComponent(study.getFirstComponent(), studyResult1);
-		componentResult21.setData(
-				"First ComponentResult's data of the second StudyResult.");
-		ComponentResult componentResult22 = jatosPublixUtils
-				.startComponent(study.getFirstComponent(), studyResult1);
-		componentResult22.setData(
-				"Second ComponentResult's data of the second StudyResult.");
-		entityManager.getTransaction().commit();
-	}
-
-	private void createTwoComponentResults(Study study)
-			throws ForbiddenReloadException {
-		entityManager.getTransaction().begin();
-		StudyResult studyResult = resultCreator.createStudyResult(study,
-				study.getDefaultBatch(), admin.getWorker());
-		// Have to set worker manually in test - don't know why
-		studyResult.setWorker(admin.getWorker());
-		// Have to set study manually in test - don't know why
-		study.getFirstComponent().setStudy(study);
-		jatosPublixUtils.startComponent(study.getFirstComponent(), studyResult);
-		jatosPublixUtils.startComponent(study.getFirstComponent(), studyResult);
-		entityManager.getTransaction().commit();
+	@After
+	public void stopApp() throws Exception {
+		// Clean up
+		testHelper.removeAllStudies();
+		testHelper.removeStudyAssetsRootDir();
 	}
 
 	@Test
@@ -94,183 +94,272 @@ public class ResultRemoverTest extends AbstractTest {
 	}
 
 	@Test
-	public void checkRemoveComponentResults()
-			throws IOException, BadRequestException, NotFoundException,
-			ForbiddenException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoComponentResults(study);
+	public void checkRemoveComponentResults() {
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<Long> idList = resultService.extractResultIds("1, 2");
-		List<ComponentResult> componentResultList = resultService
-				.getComponentResults(idList);
-		assertThat(componentResultList.size()).isEqualTo(2);
+		String ids = createTwoComponentResults(study.getId());
 
-		// Now remove them
-		entityManager.getTransaction().begin();
-		resultRemover.removeComponentResults("1, 2", admin);
-		entityManager.getTransaction().commit();
+		// Now remove both ComponentResults
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			try {
+				resultRemover.removeComponentResults(ids, admin);
+			} catch (BadRequestException | NotFoundException
+					| ForbiddenException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
 		// Check that the results are removed
-		try {
-			idList = resultService.extractResultIds("1, 2");
-			componentResultList = resultService.getComponentResults(idList);
-			Fail.fail();
-		} catch (NotFoundException e) {
-			assertThat(e.getMessage())
-					.isEqualTo(MessagesStrings.componentResultNotExist(1l));
-		}
-
-		// Clean-up
-		removeStudy(study);
+		jpaApi.withTransaction(() -> {
+			try {
+				List<Long> idList = resultService.extractResultIds("1, 2");
+				resultService.getComponentResults(idList);
+				Fail.fail();
+			} catch (NotFoundException | BadRequestException e) {
+				assertThat(e.getMessage())
+						.isEqualTo(MessagesStrings.componentResultNotExist(1l));
+			}
+		});
 	}
 
 	@Test
 	public void checkRemoveComponentResultsNotFound()
 			throws IOException, BadRequestException, ForbiddenException,
 			NotFoundException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoComponentResults(study);
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<Long> idList = resultService.extractResultIds("1, 2");
-		List<ComponentResult> componentResultList = resultService
-				.getComponentResults(idList);
-		assertThat(componentResultList.size()).isEqualTo(2);
+		String ids = createTwoComponentResults(study.getId());
 
-		// Now try to remove the results 1 and 3 (3 doesn't exist)
-		try {
-			resultRemover.removeComponentResults("1, 3", admin);
-			Fail.fail();
-		} catch (NotFoundException e) {
-			assertThat(e.getMessage())
-					.isEqualTo(MessagesStrings.componentResultNotExist(3l));
-		}
+		// Now try to remove the results but one of the result IDs doesn't exist
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			long notExistingId = 1111l;
+			try {
+				resultRemover.removeComponentResults(ids + ", " + notExistingId,
+						admin);
+				Fail.fail();
+			} catch (NotFoundException e) {
+				assertThat(e.getMessage()).isEqualTo(
+						MessagesStrings.componentResultNotExist(notExistingId));
+			} catch (ForbiddenException | BadRequestException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
-		// Check that NO result is removed - not even result 1
-		idList = resultService.extractResultIds("1, 2");
-		componentResultList = resultService.getComponentResults(idList);
-		assertThat(componentResultList.size()).isEqualTo(2);
-
-		// Clean-up
-		removeStudy(study);
+		// Check that NO result is removed - not even the two existing ones
+		jpaApi.withTransaction(() -> {
+			try {
+				List<Long> idList = resultService.extractResultIds(ids);
+				List<ComponentResult> componentResultList = resultService
+						.getComponentResults(idList);
+				assertThat(componentResultList.size()).isEqualTo(2);
+			} catch (BadRequestException | NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
 
 	@Test
-	public void checkRemoveStudyResults()
-			throws IOException, BadRequestException, NotFoundException,
-			ForbiddenException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoStudyResults(study);
+	public void checkRemoveStudyResults() {
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<Long> idList = resultService.extractResultIds("1, 2");
-		List<StudyResult> studyResultList = resultService
-				.getStudyResults(idList);
-		assertThat(studyResultList.size()).isEqualTo(2);
+		String ids = createTwoStudyResults(study.getId());
 
-		// Now remove them
-		entityManager.getTransaction().begin();
-		resultRemover.removeStudyResults("1, 2", admin);
-		entityManager.getTransaction().commit();
+		// Now remove both StudyResults
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			try {
+				resultRemover.removeStudyResults(ids, admin);
+			} catch (BadRequestException | NotFoundException
+					| ForbiddenException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
-		studyResultList = studyResultDao.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(0);
-
-		// Clean-up
-		removeStudy(study);
+		// Check that both are removed
+		jpaApi.withTransaction(() -> {
+			List<StudyResult> studyResultList = studyResultDao
+					.findAllByStudy(study);
+			assertThat(studyResultList.size()).isEqualTo(0);
+		});
 	}
 
 	@Test
 	public void checkRemoveAllStudyResults() throws IOException,
 			ForbiddenException, BadRequestException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoStudyResults(study);
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<StudyResult> studyResultList = studyResultDao
-				.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(2);
+		// Create some StudyResults
+		createTwoStudyResults(study.getId());
 
-		// Remove them
-		entityManager.getTransaction().begin();
-		resultRemover.removeAllStudyResults(study, admin);
-		entityManager.getTransaction().commit();
+		// Remove all StudyResults
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			try {
+				resultRemover.removeAllStudyResults(study, admin);
+			} catch (BadRequestException | ForbiddenException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
 		// Check that we have no more results
-		studyResultList = studyResultDao.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(0);
-
-		// Clean-up
-		removeStudy(study);
+		jpaApi.withTransaction(() -> {
+			List<StudyResult> studyResultList = studyResultDao
+					.findAllByStudy(study);
+			assertThat(studyResultList.size()).isEqualTo(0);
+		});
 	}
 
 	@Test
 	public void checkRemoveAllStudyResultsWrongUser()
 			throws IOException, BadRequestException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoStudyResults(study);
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<StudyResult> studyResultList = studyResultDao
-				.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(2);
+		// Create some StudyResults
+		createTwoStudyResults(study.getId());
 
 		// And now try to remove them with the wrong user
-		User testUser = createAndPersistUser("bla@bla.com", "Bla", "bla");
-		try {
-			resultRemover.removeAllStudyResults(study, testUser);
-			Fail.fail();
-		} catch (ForbiddenException e) {
-			assertThat(e.getMessage()).isEqualTo(MessagesStrings.studyNotUser(
-					testUser.getName(), testUser.getEmail(), study.getId(),
-					study.getTitle()));
-		}
+		jpaApi.withTransaction(() -> {
+			User testUser = testHelper.createAndPersistUser("bla@bla.com",
+					"Bla", "bla");
+			try {
+				resultRemover.removeAllStudyResults(study, testUser);
+				Fail.fail();
+			} catch (ForbiddenException e) {
+				assertThat(e.getMessage()).isEqualTo(MessagesStrings
+						.studyNotUser(testUser.getName(), testUser.getEmail(),
+								study.getId(), study.getTitle()));
+			} catch (BadRequestException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
 		// Check that we still have 2 results
-		studyResultList = studyResultDao.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(2);
-
-		// Clean-up
-		removeStudy(study);
+		jpaApi.withTransaction(() -> {
+			List<StudyResult> studyResultList = studyResultDao
+					.findAllByStudy(study);
+			assertThat(studyResultList.size()).isEqualTo(2);
+		});
 	}
 
 	@Test
 	public void checkRemoveAllStudyResultsStudyLocked()
 			throws BadRequestException, IOException, ForbiddenReloadException {
-		Study study = importExampleStudy();
-		addStudy(study);
-		createTwoStudyResults(study);
+		Study study = testHelper.createAndPersistExampleStudyForAdmin(injector);
 
-		// Check that we have 2 results
-		List<StudyResult> studyResultList = studyResultDao
-				.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(2);
+		createTwoStudyResults(study.getId());
 
 		// Lock study
-		entityManager.getTransaction().begin();
-		study.setLocked(true);
-		entityManager.getTransaction().commit();
+		jpaApi.withTransaction(() -> {
+			study.setLocked(true);
+			studyDao.update(study);
+		});
 
-		try {
-			resultRemover.removeAllStudyResults(study, admin);
-			Fail.fail();
-		} catch (ForbiddenException e) {
-			assertThat(e.getMessage())
-					.isEqualTo(MessagesStrings.studyLocked(study.getId()));
-		}
+		// Now try to remove the StudyResults from the locked study
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			try {
+				resultRemover.removeAllStudyResults(study, admin);
+				Fail.fail();
+			} catch (ForbiddenException e) {
+				assertThat(e.getMessage())
+						.isEqualTo(MessagesStrings.studyLocked(study.getId()));
+			} catch (BadRequestException e) {
+				throw new RuntimeException(e);
+			}
+		});
 
 		// Check that we still have 2 results
-		studyResultList = studyResultDao.findAllByStudy(study);
-		assertThat(studyResultList.size()).isEqualTo(2);
+		jpaApi.withTransaction(() -> {
+			List<StudyResult> studyResultList = studyResultDao
+					.findAllByStudy(study);
+			assertThat(studyResultList.size()).isEqualTo(2);
+		});
+	}
 
-		// Clean-up
-		removeStudy(study);
+	private String createTwoComponentResults(long studyId) {
+		return jpaApi.withTransaction(() -> {
+			try {
+				Study study = studyDao.findById(studyId);
+				User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+
+				StudyResult studyResult = resultCreator.createStudyResult(study,
+						study.getDefaultBatch(), admin.getWorker());
+				// Have to set worker manually in test - don't know why TODO
+				// studyResult.setWorker(admin.getWorker());
+				// Have to set study manually in test - don't know why TODO
+				// study.getFirstComponent().setStudy(study);
+				ComponentResult componentResult1 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(), studyResult);
+				ComponentResult componentResult2 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(), studyResult);
+				String ids = componentResult1.getId() + ", "
+						+ componentResult2.getId();
+
+				// Check that we have 2 results
+				List<Long> idList = resultService.extractResultIds(ids);
+				List<ComponentResult> componentResultList = resultService
+						.getComponentResults(idList);
+				assertThat(componentResultList.size()).isEqualTo(2);
+				return ids;
+			} catch (ForbiddenReloadException | BadRequestException
+					| NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		});
+	}
+
+	private String createTwoStudyResults(long studyId) {
+		return jpaApi.withTransaction(() -> {
+			try {
+				Study study = studyDao.findById(studyId);
+				User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+
+				StudyResult studyResult1 = resultCreator.createStudyResult(
+						study, study.getDefaultBatch(), admin.getWorker());
+				// Have to set worker manually in test - don't know why TODO
+				// studyResult1.setWorker(admin.getWorker());
+				ComponentResult componentResult11 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(),
+								studyResult1);
+				componentResult11.setData(
+						"First ComponentResult's data of the first StudyResult.");
+				ComponentResult componentResult12 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(),
+								studyResult1);
+				componentResult12.setData(
+						"Second ComponentResult's data of the first StudyResult.");
+
+				StudyResult studyResult2 = resultCreator.createStudyResult(
+						study, study.getBatchList().get(0), admin.getWorker());
+				// Have to set worker manually in test - don't know why TODO
+				// studyResult2.setWorker(admin.getWorker());
+				ComponentResult componentResult21 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(),
+								studyResult1);
+				componentResult21.setData(
+						"First ComponentResult's data of the second StudyResult.");
+				ComponentResult componentResult22 = jatosPublixUtils
+						.startComponent(study.getFirstComponent(),
+								studyResult1);
+				componentResult22.setData(
+						"Second ComponentResult's data of the second StudyResult.");
+
+				String ids = studyResult1.getId() + ", " + studyResult2.getId();
+
+				// Check that we have 2 results
+				List<Long> idList = resultService.extractResultIds(ids);
+				List<StudyResult> studyResultList = resultService
+						.getStudyResults(idList);
+				assertThat(studyResultList.size()).isEqualTo(2);
+
+				return ids;
+			} catch (ForbiddenReloadException | BadRequestException
+					| NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
 
 }
