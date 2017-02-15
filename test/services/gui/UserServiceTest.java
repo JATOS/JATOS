5baa50d52a69fdb3e064e417161563403d90d160
kristian-lange <lange.kristian@gmail.com>
2017-02-15 19:21:15 +0100

    Fix tests in UserServiceTest after Play upgrade

--- test/services/gui/UserServiceTest.java
+++ test/services/gui/UserServiceTest.java
@@ -4,15 +4,27 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.fest.assertions.Fail;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import exceptions.gui.ForbiddenException;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import daos.common.UserDao;
 import exceptions.gui.NotFoundException;
-import general.AbstractTest;
+import general.TestHelper;
 import general.common.MessagesStrings;
 import models.common.User;
+import play.ApplicationLoader;
+import play.Environment;
 import play.data.validation.ValidationError;
+import play.db.jpa.JPAApi;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.guice.GuiceApplicationLoader;
 import utils.common.HashUtils;
 
 /**
@@ -20,14 +32,35 @@ import utils.common.HashUtils;
  * 
  * @author Kristian Lange
  */
-public class UserServiceTest extends AbstractTest {
+public class UserServiceTest {
 
-	@Override
-	public void before() throws Exception {
+	private Injector injector;
+
+	@Inject
+	private TestHelper testHelper;
+
+	@Inject
+	private JPAApi jpaApi;
+
+	@Inject
+	private UserService userService;
+
+	@Inject
+	private UserDao userDao;
+
+	@Before
+	public void startApp() throws Exception {
+		GuiceApplicationBuilder builder = new GuiceApplicationLoader()
+				.builder(new ApplicationLoader.Context(Environment.simple()));
+		injector = Guice.createInjector(builder.applicationModule());
+		injector.injectMembers(this);
 	}
 
-	@Override
-	public void after() throws Exception {
+	@After
+	public void stopApp() throws Exception {
+		// Clean up
+		testHelper.removeAllStudies();
+		testHelper.removeStudyAssetsRootDir();
 	}
 
 	@Test
@@ -38,44 +71,28 @@ public class UserServiceTest extends AbstractTest {
 
 	@Test
 	public void checkRetrieveUser() {
-		User user = null;
-		entityManager.getTransaction().begin();
-		try {
-			user = userService.retrieveUser("admin");
-		} catch (NotFoundException e) {
-			Fail.fail();
-		}
-		entityManager.getTransaction().commit();
-		assertThat(user).isEqualTo(admin);
+		// Check retrieval of admin user
+		jpaApi.withTransaction(() -> {
+			User admin = testHelper.getAdmin();
+			User user = null;
+			try {
+				user = userService.retrieveUser("admin");
+			} catch (NotFoundException e) {
+				Fail.fail();
+			}
+			assertThat(user).isEqualTo(admin);
+		});
 
-		entityManager.getTransaction().begin();
-		try {
-			userService.retrieveUser("bla");
-			Fail.fail();
-		} catch (NotFoundException e) {
-			assertThat(e.getMessage())
-					.isEqualTo(MessagesStrings.userNotExist("bla"));
-		}
-		entityManager.getTransaction().commit();
-	}
-
-	@Test
-	public void testCheckUserLoggedIn() {
-		try {
-			checker.checkUserLoggedIn(admin, admin);
-		} catch (ForbiddenException e) {
-			Fail.fail();
-		}
-
-		User testUser = createAndPersistUser("bla@bla.com", "Bla", "bla");
-
-		try {
-			checker.checkUserLoggedIn(testUser, admin);
-			Fail.fail();
-		} catch (ForbiddenException e) {
-			assertThat(e.getMessage()).isEqualTo(
-					MessagesStrings.userMustBeLoggedInToSeeProfile(testUser));
-		}
+		// Unknown user should throw NotFoundException
+		jpaApi.withTransaction(() -> {
+			try {
+				userService.retrieveUser("bla");
+				Fail.fail();
+			} catch (NotFoundException e) {
+				assertThat(e.getMessage())
+						.isEqualTo(MessagesStrings.userNotExist("bla"));
+			}
+		});
 	}
 
 	@Test
@@ -86,45 +103,66 @@ public class UserServiceTest extends AbstractTest {
 		} catch (RuntimeException e) {
 			Fail.fail();
 		}
-		assertThat(hash).isNotEmpty();
+		assertThat(hash).isEqualTo("128ecf542a35ac5270a87dc740918404");
 	}
 
 	@Test
 	public void checkValidateNewUser() {
 		User testUser = new User("bla@bla.com", "Bla", "bla");
-		List<ValidationError> errorList = userService.validateNewUser(testUser,
-				"bla", "bla");
-		assertThat(errorList).isEmpty();
 
-		errorList = userService.validateNewUser(testUser, "", "foo");
-		assertThat(errorList).hasSize(2);
-		assertThat(errorList.get(0).message())
-				.isEqualTo(MessagesStrings.PASSWORDS_SHOULDNT_BE_EMPTY_STRINGS);
-		assertThat(errorList.get(1).message())
-				.isEqualTo(MessagesStrings.PASSWORDS_DONT_MATCH);
+		jpaApi.withTransaction(() -> {
+			List<ValidationError> errorList = userService
+					.validateNewUser(testUser, "bla", "bla");
+			assertThat(errorList).isEmpty();
+		});
 
-		errorList = userService.validateNewUser(testUser, "bla", "foo");
-		assertThat(errorList).hasSize(1);
-		assertThat(errorList.get(0).message())
-				.isEqualTo(MessagesStrings.PASSWORDS_DONT_MATCH);
+		jpaApi.withTransaction(() -> {
+			List<ValidationError> errorList = userService
+					.validateNewUser(testUser, "", "foo");
+			assertThat(errorList).hasSize(2);
+			assertThat(errorList.get(0).message()).isEqualTo(
+					MessagesStrings.PASSWORDS_SHOULDNT_BE_EMPTY_STRINGS);
+			assertThat(errorList.get(1).message())
+					.isEqualTo(MessagesStrings.PASSWORDS_DONT_MATCH);
+		});
 
-		errorList = userService.validateNewUser(admin, "bla", "bla");
-		assertThat(errorList).hasSize(1);
-		assertThat(errorList.get(0).message())
-				.isEqualTo(MessagesStrings.THIS_EMAIL_IS_ALREADY_REGISTERED);
+		jpaApi.withTransaction(() -> {
+			List<ValidationError> errorList = userService
+					.validateNewUser(testUser, "bla", "foo");
+			assertThat(errorList).hasSize(1);
+			assertThat(errorList.get(0).message())
+					.isEqualTo(MessagesStrings.PASSWORDS_DONT_MATCH);
+		});
+
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			List<ValidationError> errorList = userService.validateNewUser(admin,
+					"bla", "bla");
+			assertThat(errorList).hasSize(1);
+			assertThat(errorList.get(0).message()).isEqualTo(
+					MessagesStrings.THIS_EMAIL_IS_ALREADY_REGISTERED);
+		});
 	}
 
 	@Test
 	public void checkValidateChangePassword() {
-		List<ValidationError> errorList = userService.validateChangePassword(
-				admin, "bla", "bla", admin.getPasswordHash());
-		assertThat(errorList).isEmpty();
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			List<ValidationError> errorList = userService
+					.validateChangePassword(admin, "bla", "bla",
+							admin.getPasswordHash());
+			assertThat(errorList).isEmpty();
+		});
 
-		errorList = userService.validateChangePassword(admin, "bla", "bla",
-				"wrongPasswordhash");
-		assertThat(errorList).hasSize(1);
-		assertThat(errorList.get(0).message())
-				.isEqualTo(MessagesStrings.WRONG_OLD_PASSWORD);
+		jpaApi.withTransaction(() -> {
+			User admin = userDao.findByEmail(UserService.ADMIN_EMAIL);
+			List<ValidationError> errorList = userService
+					.validateChangePassword(admin, "bla", "bla",
+							"wrongPasswordhash");
+			assertThat(errorList).hasSize(1);
+			assertThat(errorList.get(0).message())
+					.isEqualTo(MessagesStrings.WRONG_OLD_PASSWORD);
+		});
 	}
 
 }
