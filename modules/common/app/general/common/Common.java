93082acc9d720a88ebfb87bb4bf2e700c38e22fb
kristian-lange <lange.kristian@gmail.com>
2017-10-21 21:58:29 +0200

    Added jatos.userSession.validation toggle

--- modules/common/app/general/common/Common.java
+++ modules/common/app/general/common/Common.java
@@ -1,14 +1,13 @@
 package general.common;
 
-import java.io.File;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import play.Configuration;
 import play.Logger;
 import play.Logger.ALogger;
 import play.api.Application;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.File;
 
 /**
  * This class provides configuration that is common to all modules of JATOS. It
@@ -16,159 +15,173 @@ import play.api.Application;
  * start (triggered in GuiceConfig). Since most fields are initialized by the
  * constructor during the JATOS' start (triggered in GuiceConfig), it's save to
  * access them via static getter methods.
- * 
+ *
  * @author Kristian Lange
  */
 @Singleton
 public class Common {
 
-	private static final ALogger LOGGER = Logger.of(Common.class);
+    private static final ALogger LOGGER = Logger.of(Common.class);
 
-	/**
-	 * JATOS version
-	 */
-	public static final String JATOS_VERSION = Common.class.getPackage()
-			.getImplementationVersion();
+    /**
+     * JATOS version
+     */
+    public static final String JATOS_VERSION = Common.class.getPackage()
+            .getImplementationVersion();
 
-	/**
-	 * Property name in application config for the path in the file system to
-	 * the study assets root directory, the directory where all study assets are
-	 * located
-	 */
-	private static final String PROPERTY_STUDY_ASSETS_ROOT_PATH = "jatos.studyAssetsRootPath";
+    /**
+     * Property name in application config for the path in the file system to
+     * the study assets root directory, the directory where all study assets are
+     * located
+     */
+    private static final String PROPERTY_STUDY_ASSETS_ROOT_PATH = "jatos.studyAssetsRootPath";
 
-	/**
-	 * JATOS' absolute base path without trailing '/.'
-	 */
-	private static String basepath;
+    /**
+     * JATOS' absolute base path without trailing '/.'
+     */
+    private static String basepath;
 
-	/**
-	 * Path in the file system to the study assets root directory. If the
-	 * property is defined in the configuration file then use it as the base
-	 * path. If property isn't defined, try in default study path instead.
-	 */
-	private static String studyAssetsRootPath;
+    /**
+     * Path in the file system to the study assets root directory. If the
+     * property is defined in the configuration file then use it as the base
+     * path. If property isn't defined, try in default study path instead.
+     */
+    private static String studyAssetsRootPath;
 
-	/**
-	 * Is true if an in-memory database is used.
-	 */
-	private static boolean inMemoryDb;
+    /**
+     * Is true if an in-memory database is used.
+     */
+    private static boolean inMemoryDb;
 
-	/**
-	 * Time in minutes when the Play session will timeout (defined in
-	 * application.conf)
-	 */
-	private static int userSessionTimeout;
+    /**
+     * Time in minutes when the Play session will timeout (defined in
+     * application.conf)
+     */
+    private static int userSessionTimeout;
 
-	/**
-	 * Time in minutes a user can be inactive before he will be logged-out
-	 * (defined in application.conf)
-	 */
-	private static int userSessionInactivity;
+    /**
+     * Time in minutes a user can be inactive before he will be logged-out
+     * (defined in application.conf)
+     */
+    private static int userSessionInactivity;
 
-	/**
-	 * Database URL as defined in application.conf
-	 */
-	private static String dbDefaultUrl;
+    /**
+     * Toggle for user session validation (not the Play session validation which is done by Play).
+     */
+    private static boolean userSessionValidation;
 
-	/**
-	 * Database driver as defined in application.conf
-	 */
-	private static String dbDefaultDriver;
+    /**
+     * Database URL as defined in application.conf
+     */
+    private static String dbDefaultUrl;
 
-	/**
-	 * JPA persistence unit as defined in application.conf
-	 */
-	private static String jpaDefault;
+    /**
+     * Database driver as defined in application.conf
+     */
+    private static String dbDefaultDriver;
 
-	@Inject
-	Common(Application application, Configuration configuration) {
-		basepath = fillBasePath(application);
-		studyAssetsRootPath = fillStudyAssetsRootPath(configuration);
-		inMemoryDb = configuration.getString("db.default.url")
-				.contains("jdbc:h2:mem:");
-		userSessionTimeout = configuration.getInt("jatos.userSession.timeout");
-		userSessionInactivity = configuration.getInt("jatos.userSession.inactivity");
-		dbDefaultUrl = configuration.getString("db.default.url");
-		dbDefaultDriver = configuration.getString("db.default.driver");
-		jpaDefault = configuration.getString("jpa.default");
-	}
+    /**
+     * JPA persistence unit as defined in application.conf
+     */
+    private static String jpaDefault;
 
-	private String fillBasePath(Application application) {
-		String tempBasePath = application.path().getAbsolutePath();
-		if (tempBasePath.endsWith(File.separator + ".")) {
-			tempBasePath = tempBasePath.substring(0, tempBasePath.length() - 2);
-		}
-		if (tempBasePath.endsWith(File.separator)) {
-			tempBasePath = tempBasePath.substring(0, tempBasePath.length() - 1);
-		}
-		return tempBasePath;
-	}
+    @Inject
+    Common(Application application, Configuration configuration) {
+        basepath = fillBasePath(application);
+        studyAssetsRootPath = fillStudyAssetsRootPath(configuration);
+        inMemoryDb = configuration.getString("db.default.url")
+                .contains("jdbc:h2:mem:");
+        userSessionTimeout = configuration.getInt("jatos.userSession.timeout");
+        userSessionInactivity = configuration.getInt("jatos.userSession.inactivity");
+        userSessionValidation = configuration.getBoolean("jatos.userSession.validation");
+        if (!userSessionValidation) {
+            LOGGER.warn("WARNING - User session validation is switched off. " +
+                    "This decreases security. Proceed only if you know what you are doing.");
+        }
+        dbDefaultUrl = configuration.getString("db.default.url");
+        dbDefaultDriver = configuration.getString("db.default.driver");
+        jpaDefault = configuration.getString("jpa.default");
+    }
 
-	private String fillStudyAssetsRootPath(Configuration configuration) {
-		String tempStudyAssetsRootPath = configuration
-				.getString(PROPERTY_STUDY_ASSETS_ROOT_PATH);
-		if (tempStudyAssetsRootPath == null
-				|| tempStudyAssetsRootPath.trim().isEmpty()) {
-			LOGGER.error(
-					"Missing configuration of path to study assets directory: "
-							+ "It must be set in application.conf under "
-							+ PROPERTY_STUDY_ASSETS_ROOT_PATH + ".");
-			System.exit(1);
-		}
+    private String fillBasePath(Application application) {
+        String tempBasePath = application.path().getAbsolutePath();
+        if (tempBasePath.endsWith(File.separator + ".")) {
+            tempBasePath = tempBasePath.substring(0, tempBasePath.length() - 2);
+        }
+        if (tempBasePath.endsWith(File.separator)) {
+            tempBasePath = tempBasePath.substring(0, tempBasePath.length() - 1);
+        }
+        return tempBasePath;
+    }
 
-		// Replace ~ with actual home directory
-		tempStudyAssetsRootPath = tempStudyAssetsRootPath.replace("~",
-				System.getProperty("user.home"));
-		// Replace Unix-like file separator with actual system's one
-		tempStudyAssetsRootPath = tempStudyAssetsRootPath.replace("/",
-				File.separator);
+    private String fillStudyAssetsRootPath(Configuration configuration) {
+        String tempStudyAssetsRootPath = configuration
+                .getString(PROPERTY_STUDY_ASSETS_ROOT_PATH);
+        if (tempStudyAssetsRootPath == null
+                || tempStudyAssetsRootPath.trim().isEmpty()) {
+            LOGGER.error(
+                    "Missing configuration of path to study assets directory: "
+                            + "It must be set in application.conf under "
+                            + PROPERTY_STUDY_ASSETS_ROOT_PATH + ".");
+            System.exit(1);
+        }
 
-		// If relative path add JATOS' base path as prefix
+        // Replace ~ with actual home directory
+        tempStudyAssetsRootPath = tempStudyAssetsRootPath.replace("~",
+                System.getProperty("user.home"));
+        // Replace Unix-like file separator with actual system's one
+        tempStudyAssetsRootPath = tempStudyAssetsRootPath.replace("/",
+                File.separator);
 
-		if (!(new File(tempStudyAssetsRootPath).isAbsolute())) {
-			tempStudyAssetsRootPath = basepath + File.separator
-					+ tempStudyAssetsRootPath;
-		}
-		LOGGER.info(
-				"Path to study assets directory is " + tempStudyAssetsRootPath);
-		return tempStudyAssetsRootPath;
-	}
+        // If relative path add JATOS' base path as prefix
 
-	public static String getJatosVersion() {
-		return JATOS_VERSION;
-	}
+        if (!(new File(tempStudyAssetsRootPath).isAbsolute())) {
+            tempStudyAssetsRootPath = basepath + File.separator
+                    + tempStudyAssetsRootPath;
+        }
+        LOGGER.info(
+                "Path to study assets directory is " + tempStudyAssetsRootPath);
+        return tempStudyAssetsRootPath;
+    }
 
-	public static String getBasepath() {
-		return basepath;
-	}
+    public static String getJatosVersion() {
+        return JATOS_VERSION;
+    }
 
-	public static String getStudyAssetsRootPath() {
-		return studyAssetsRootPath;
-	}
+    public static String getBasepath() {
+        return basepath;
+    }
 
-	public static boolean isInMemoryDb() {
-		return inMemoryDb;
-	}
+    public static String getStudyAssetsRootPath() {
+        return studyAssetsRootPath;
+    }
 
-	public static int getUserSessionTimeout() {
-		return userSessionTimeout;
-	}
+    public static boolean isInMemoryDb() {
+        return inMemoryDb;
+    }
 
-	public static int getUserSessionInactivity() {
-		return userSessionInactivity;
-	}
+    public static int getUserSessionTimeout() {
+        return userSessionTimeout;
+    }
 
-	public static String getDbDefaultUrl() {
-		return dbDefaultUrl;
-	}
+    public static int getUserSessionInactivity() {
+        return userSessionInactivity;
+    }
 
-	public static String getDbDefaultDriver() {
-		return dbDefaultDriver;
-	}
+    public static boolean getUserSessionValidation() {
+        return userSessionValidation;
+    }
 
-	public static String getJpaDefault() {
-		return jpaDefault;
-	}
+    public static String getDbDefaultUrl() {
+        return dbDefaultUrl;
+    }
+
+    public static String getDbDefaultDriver() {
+        return dbDefaultDriver;
+    }
+
+    public static String getJpaDefault() {
+        return jpaDefault;
+    }
 
 }
--- modules/gui/app/controllers/gui/actionannotations/AuthenticationAction.java
+++ modules/gui/app/controllers/gui/actionannotations/AuthenticationAction.java
@@ -1,18 +1,9 @@
 package controllers.gui.actionannotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-
-import javax.inject.Inject;
-import javax.inject.Provider;
-
 import controllers.gui.Authentication;
 import controllers.gui.Home;
 import controllers.gui.actionannotations.AuthenticationAction.Authenticated;
+import general.common.Common;
 import general.common.RequestScope;
 import general.gui.FlashScopeMessaging;
 import general.gui.RequestScopeMessaging;
@@ -26,6 +17,15 @@ import play.mvc.Result;
 import play.mvc.With;
 import services.gui.AuthenticationService;
 import utils.common.HttpUtils;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 /**
  * This class defines the @Authenticated annotation used in JATOS GUI
@@ -60,161 +60,148 @@ import utils.common.HttpUtils;
  */
 public class AuthenticationAction extends Action<Authenticated> {
 
-	/**
-	 * This @Authenticated annotation can be used on every controller action
-	 * where authentication and authorization is required. If no Role is added
-	 * than the default Role 'USER' is assumed.
-	 */
-	@With(AuthenticationAction.class)
-	@Target({ElementType.TYPE, ElementType.METHOD})
-	@Retention(RetentionPolicy.RUNTIME)
-	public @interface Authenticated {
-		Role value() default Role.USER;
-	}
+    /**
+     * This @Authenticated annotation can be used on every controller action
+     * where authentication and authorization is required. If no Role is added
+     * than the default Role 'USER' is assumed.
+     */
+    @With(AuthenticationAction.class)
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Authenticated {
+        Role value() default Role.USER;
+    }
 
-	private static final ALogger LOGGER = Logger.of(AuthenticationAction.class);
+    private static final ALogger LOGGER = Logger.of(AuthenticationAction.class);
 
-	private final Provider<Home> homeProvider;
-	private final AuthenticationService authenticationService;
+    private final Provider<Home> homeProvider;
+    private final AuthenticationService authenticationService;
 
-	@Inject
-	AuthenticationAction(Provider<Home> homeProvider,
-			AuthenticationService authenticationService) {
-		this.homeProvider = homeProvider;
-		this.authenticationService = authenticationService;
-	}
+    @Inject
+    AuthenticationAction(Provider<Home> homeProvider, AuthenticationService authenticationService) {
+        this.homeProvider = homeProvider;
+        this.authenticationService = authenticationService;
+    }
 
-	public CompletionStage<Result> call(Http.Context ctx) {
-		// For authentication it's actually enough to check that the email is in
-		// Play's session. Play's session is safe from tempering. But we
-		// retrieve the user from the database and put it into our RequestScope
-		// since we need it later anyway. Storing it in the RequestScope now
-		// saves us some database requests later.
-		User loggedInUser = authenticationService
-				.getLoggedInUserBySessionCookie(ctx.session());
-		if (loggedInUser == null) {
-			authenticationService.clearSessionCookie(ctx.session());
-			return callForbiddenDueToAuthentication(
-					ctx.request().remoteAddress(),
-					ctx.request().path());
-		}
-		RequestScope.put(AuthenticationService.LOGGED_IN_USER, loggedInUser);
+    public CompletionStage<Result> call(Http.Context ctx) {
+        // For authentication it's actually enough to check that the email is in
+        // Play's session. Play's session is safe from tempering. But we
+        // retrieve the user from the database and put it into our RequestScope
+        // since we need it later anyway. Storing it in the RequestScope now
+        // saves us some database requests later.
+        User loggedInUser = authenticationService.getLoggedInUserBySessionCookie(ctx.session());
+        if (loggedInUser == null) {
+            authenticationService.clearSessionCookie(ctx.session());
+            return callForbiddenDueToAuthentication(ctx.request().remoteAddress(),
+                    ctx.request().path());
+        }
+        RequestScope.put(AuthenticationService.LOGGED_IN_USER, loggedInUser);
 
-		// Check user's session ID
-		if (!authenticationService.isValidSessionId(ctx.session(),
-				loggedInUser.getEmail(), ctx.request().remoteAddress())) {
-			authenticationService.clearSessionCookie(ctx.session());
-			return callForbiddenDueToInvalidSession(loggedInUser.getEmail(),
-					ctx.request().remoteAddress(), ctx.request().path());
-		}
+        // Check user's session ID (only if not switched off in configuration)
+        if (Common.getUserSessionValidation() &&
+                !authenticationService.isValidSessionId(ctx.session(),
+                        loggedInUser.getEmail(), ctx.request().remoteAddress())) {
+            authenticationService.clearSessionCookie(ctx.session());
+            return callForbiddenDueToInvalidSession(loggedInUser.getEmail(),
+                    ctx.request().remoteAddress(), ctx.request().path());
+        }
 
-		// Check session timeout
-		if (authenticationService.isSessionTimeout(ctx.session())) {
-			authenticationService.clearSessionCookieAndSessionCache(
-					ctx.session(), loggedInUser.getEmail(),
-					ctx.request().remoteAddress());
-			return callForbiddenDueToSessionTimeout(loggedInUser.getEmail());
-		}
+        // Check session timeout
+        if (authenticationService.isSessionTimeout(ctx.session())) {
+            authenticationService.clearSessionCookieAndSessionCache(
+                    ctx.session(), loggedInUser.getEmail(), ctx.request().remoteAddress());
+            return callForbiddenDueToSessionTimeout(loggedInUser.getEmail());
+        }
 
-		// Check inactivity timeout
-		if (authenticationService.isInactivityTimeout(ctx.session())) {
-			authenticationService.clearSessionCookieAndSessionCache(
-					ctx.session(), loggedInUser.getEmail(),
-					ctx.request().remoteAddress());
-			return callForbiddenDueToInactivityTimeout(loggedInUser.getEmail());
-		}
+        // Check inactivity timeout
+        if (authenticationService.isInactivityTimeout(ctx.session())) {
+            authenticationService.clearSessionCookieAndSessionCache(
+                    ctx.session(), loggedInUser.getEmail(), ctx.request().remoteAddress());
+            return callForbiddenDueToInactivityTimeout(loggedInUser.getEmail());
+        }
 
-		authenticationService.refreshSessionCookie(ctx.session());
+        authenticationService.refreshSessionCookie(ctx.session());
 
-		// Check authorization
-		if (!isAuthorized(loggedInUser)) {
-			return callForbiddenDueToAuthorization(loggedInUser.getEmail(),
-					ctx.request().path());
-		}
+        // Check authorization
+        if (!isAuthorized(loggedInUser)) {
+            return callForbiddenDueToAuthorization(loggedInUser.getEmail(), ctx.request().path());
+        }
 
-		// Everything ok: authenticated and authorized
-		return delegate.call(ctx);
-	}
+        // Everything ok: authenticated and authorized
+        return delegate.call(ctx);
+    }
 
-	private boolean isAuthorized(User loggedInUser) {
-		// configuration.value() contains the Role parameter of @Authenticated
-		Role neededRole = configuration.value();
-		return loggedInUser.hasRole(neededRole);
-	}
+    private boolean isAuthorized(User loggedInUser) {
+        // configuration.value() contains the Role parameter of @Authenticated
+        Role neededRole = configuration.value();
+        return loggedInUser.hasRole(neededRole);
+    }
 
-	private CompletionStage<Result> callForbiddenDueToAuthentication(
-			String remoteAddress, String urlPath) {
-		LOGGER.warn("Authentication failed: remote address " + remoteAddress
-				+ " tried to access page " + urlPath);
-		if (HttpUtils.isAjax()) {
-			return CompletableFuture
-					.completedFuture(forbidden("Not logged in"));
-		}
-		if (!urlPath.isEmpty() && !urlPath.matches("(/|/jatos|/jatos/)")) {
-			FlashScopeMessaging.error(
-					"You are not allowed to access this page. Please log in.");
-		}
-		return CompletableFuture.completedFuture(
-				redirect(controllers.gui.routes.Authentication.login()));
-	}
+    private CompletionStage<Result> callForbiddenDueToAuthentication(
+            String remoteAddress, String urlPath) {
+        LOGGER.warn("Authentication failed: remote address " + remoteAddress
+                + " tried to access page " + urlPath);
+        if (HttpUtils.isAjax()) {
+            return CompletableFuture.completedFuture(forbidden("Not logged in"));
+        }
+        if (!urlPath.isEmpty() && !urlPath.matches("(/|/jatos|/jatos/)")) {
+            FlashScopeMessaging.error("You are not allowed to access this page. Please log in.");
+        }
+        return CompletableFuture.completedFuture(
+                redirect(controllers.gui.routes.Authentication.login()));
+    }
 
-	private CompletionStage<Result> callForbiddenDueToInvalidSession(
-			String userEmail, String remoteAddress, String urlPath) {
-		LOGGER.warn("Invalid session: user " + userEmail
-				+ " tried to access page " + urlPath + " from remote address "
-				+ remoteAddress + ".");
-		if (HttpUtils.isAjax()) {
-			return CompletableFuture
-					.completedFuture(forbidden("Invalid session"));
-		} else {
-			FlashScopeMessaging.warning("You have been logged out.");
-			return CompletableFuture.completedFuture(
-					redirect(controllers.gui.routes.Authentication.login()));
-		}
-	}
+    private CompletionStage<Result> callForbiddenDueToInvalidSession(
+            String userEmail, String remoteAddress, String urlPath) {
+        LOGGER.warn("Invalid session: user " + userEmail
+                + " tried to access page " + urlPath + " from remote address "
+                + remoteAddress + ".");
+        if (HttpUtils.isAjax()) {
+            return CompletableFuture.completedFuture(forbidden("Invalid session"));
+        } else {
+            FlashScopeMessaging.warning("You have been logged out.");
+            return CompletableFuture.completedFuture(
+                    redirect(controllers.gui.routes.Authentication.login()));
+        }
+    }
 
-	private CompletionStage<Result> callForbiddenDueToSessionTimeout(
-			String userEmail) {
-		LOGGER.info("Session of user " + userEmail
-				+ " has expired and the user has been logged out.");
-		if (HttpUtils.isAjax()) {
-			return CompletableFuture
-					.completedFuture(forbidden("Session timeout"));
-		} else {
-			FlashScopeMessaging.success(
-					"Your session has expired. You have been logged out.");
-			return CompletableFuture.completedFuture(
-					redirect(controllers.gui.routes.Authentication.login()));
-		}
-	}
+    private CompletionStage<Result> callForbiddenDueToSessionTimeout(
+            String userEmail) {
+        LOGGER.info("Session of user " + userEmail
+                + " has expired and the user has been logged out.");
+        if (HttpUtils.isAjax()) {
+            return CompletableFuture.completedFuture(forbidden("Session timeout"));
+        } else {
+            FlashScopeMessaging.success("Your session has expired. You have been logged out.");
+            return CompletableFuture.completedFuture(
+                    redirect(controllers.gui.routes.Authentication.login()));
+        }
+    }
 
-	private CompletionStage<Result> callForbiddenDueToInactivityTimeout(
-			String userEmail) {
-		LOGGER.info("User " + userEmail
-				+ " has been logged out due to inactivity.");
-		if (HttpUtils.isAjax()) {
-			return CompletableFuture
-					.completedFuture(forbidden("Inactivity timeout"));
-		} else {
-			FlashScopeMessaging
-					.success("You have been logged out due to inactivity.");
-			return CompletableFuture.completedFuture(
-					redirect(controllers.gui.routes.Authentication.login()));
-		}
-	}
+    private CompletionStage<Result> callForbiddenDueToInactivityTimeout(String userEmail) {
+        LOGGER.info("User " + userEmail
+                + " has been logged out due to inactivity.");
+        if (HttpUtils.isAjax()) {
+            return CompletableFuture.completedFuture(forbidden("Inactivity timeout"));
+        } else {
+            FlashScopeMessaging.success("You have been logged out due to inactivity.");
+            return CompletableFuture.completedFuture(
+                    redirect(controllers.gui.routes.Authentication.login()));
+        }
+    }
 
-	private CompletionStage<Result> callForbiddenDueToAuthorization(
-			String userEmail, String urlPath) {
-		String message = "User " + userEmail + " isn't allowed to access page "
-				+ urlPath + ".";
-		LOGGER.warn(message);
-		if (HttpUtils.isAjax()) {
-			return CompletableFuture.completedFuture(forbidden(message));
-		} else {
-			RequestScopeMessaging.error(message);
-			return CompletableFuture.completedFuture(
-					homeProvider.get().home(Http.Status.FORBIDDEN));
-		}
-	}
+    private CompletionStage<Result> callForbiddenDueToAuthorization(
+            String userEmail, String urlPath) {
+        String message = "User " + userEmail + " isn't allowed to access page "
+                + urlPath + ".";
+        LOGGER.warn(message);
+        if (HttpUtils.isAjax()) {
+            return CompletableFuture.completedFuture(forbidden(message));
+        } else {
+            RequestScopeMessaging.error(message);
+            return CompletableFuture.completedFuture(
+                    homeProvider.get().home(Http.Status.FORBIDDEN));
+        }
+    }
 
 }
