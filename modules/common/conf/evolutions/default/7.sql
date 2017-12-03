dc27ed522dae2309fa1665dd35cde6055cc51508
kristian-lange <lange.kristian@gmail.com>
2017-12-03 19:59:06 +0100

    User's emails to lower case too and disable foreign key checks during evolution

--- modules/common/conf/evolutions/default/7.sql
+++ modules/common/conf/evolutions/default/7.sql
@@ -1,6 +1,11 @@
 # --- !Ups
+SET FOREIGN_KEY_CHECKS = 0;
+
 UPDATE `StudyUserMap` SET user_email = LOWER(user_email);
 UPDATE `User_roleList` SET User_email = LOWER(User_email);
+UPDATE `User` SET email = LOWER(email);
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 # --- !Downs
 # --- not supported
