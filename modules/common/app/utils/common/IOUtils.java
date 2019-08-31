8df26ca3b7ade88b7025fd9d292485edf08106ae
kristian <kristian.lange@talentwunder.com>
2019-08-31 17:00:03 +0200

    IOUtils.java - ZIP file suffix .zip -> .jzip

--- modules/common/app/utils/common/IOUtils.java
+++ modules/common/app/utils/common/IOUtils.java
@@ -22,14 +22,13 @@ import java.nio.file.Paths;
 @Singleton
 public class IOUtils {
 
-    public static final String STUDY_FILE_SUFFIX     = "jas";
+    public static final String STUDY_FILE_SUFFIX = "jas";
     public static final String COMPONENT_FILE_SUFFIX = "jac";
-    public static final String ZIP_FILE_SUFFIX       = "zip";
+    public static final String ZIP_FILE_SUFFIX = "jzip";
 
     /**
-     * Regular expression of illegal characters or strings in file or directory
-     * names '/', '\n', '\r', * '//', '\t', '\0', '\f', '`', '?', '*', '\\',
-     * '<', '>', '|', '\"', ':', '~', '!', '§', '$', '%', '&'
+     * Regular expression of illegal characters or strings in file or directory names '/', '\n', '\r', * '//', '\t',
+     * '\0', '\f', '`', '?', '*', '\\', '<', '>', '|', '\"', ':', '~', '!', '§', '$', '%', '&'
      */
     public static final String REGEX_ILLEGAL_IN_FILENAME = "[\\s\\n\\r\\t\\f*?\"\\\\\0/,`<>|:~!§$%&^°]";
 
@@ -55,17 +54,14 @@ public class IOUtils {
     }
 
     /**
-     * Gets the File object (can be an directory) while preventing a path
-     * traversal attack. path and filePath together build the full path (like
-     * path/filePath). path must be a directory.
+     * Gets the File object (can be an directory) while preventing a path traversal attack. path and filePath together
+     * build the full path (like path/filePath). path must be a directory.
      */
-    private File getFileSecurely(String baseDirPathStr, String filePathStr)
-            throws IOException {
+    private File getFileSecurely(String baseDirPathStr, String filePathStr) throws IOException {
         Path baseDirPath = Paths.get(baseDirPathStr);
         Path filePath = Paths.get(filePathStr);
         if (!baseDirPath.isAbsolute()) {
-            throw new IOException(
-                    MessagesStrings.pathNotAbsolute(baseDirPathStr));
+            throw new IOException(MessagesStrings.pathNotAbsolute(baseDirPathStr));
         }
         if (filePath.isAbsolute()) {
             throw new IOException(MessagesStrings.pathNotRelative(filePathStr));
@@ -78,16 +74,15 @@ public class IOUtils {
         // Make sure the resulting path is still within the required directory.
         // (In the example above, "/foo/bar/attack" is not.)
         if (!resolvedPath.startsWith(baseDirPath)) {
-            throw new IOException(MessagesStrings
-                    .couldntGeneratePathToFileOrDir(filePathStr));
+            throw new IOException(MessagesStrings.couldntGeneratePathToFileOrDir(filePathStr));
         }
 
         return resolvedPath.toFile();
     }
 
     /**
-     * Gets the File object of the directory while preventing a path traversal
-     * attack and checks if the directory actually exists.
+     * Gets the File object of the directory while preventing a path traversal attack and checks if the directory
+     * actually exists.
      */
     private File getExistingDirSecurely(String fullPathStr) throws IOException {
         Path fullPath = Paths.get(fullPathStr);
@@ -99,8 +94,7 @@ public class IOUtils {
         // (e.g. "/foo/bar/baz/../attack" -> "/foo/bar/attack")
         // and check that the normalized path is equal to the original one
         if (!fullPath.normalize().equals(fullPath)) {
-            throw new IOException(MessagesStrings
-                    .couldntGeneratePathToFileOrDir(fullPathStr));
+            throw new IOException(MessagesStrings.couldntGeneratePathToFileOrDir(fullPathStr));
         }
         File file = fullPath.toFile();
         if (!file.exists() || !file.isDirectory()) {
@@ -115,11 +109,10 @@ public class IOUtils {
     }
 
     /**
-     * Gets the File object while preventing a path traversal attack and checks
-     * whether the file exists and is no directory.
+     * Gets the File object while preventing a path traversal attack and checks whether the file exists and is no
+     * directory.
      */
-    public File getExistingFileSecurely(String path, String filePath)
-            throws IOException {
+    public File getExistingFileSecurely(String path, String filePath) throws IOException {
         File file = getFileSecurely(path, filePath);
         if (file == null || !file.exists() || file.isDirectory()) {
             throw new IOException(MessagesStrings.FILE_NOT_EXIST_OR_DIR);
@@ -128,11 +121,9 @@ public class IOUtils {
     }
 
     /**
-     * Gets the File object which resides under filePath within the study
-     * assets' directory.
+     * Gets the File object which resides under filePath within the study assets' directory.
      */
-    public File getFileInStudyAssetsDir(String dirName, String filePath)
-            throws IOException {
+    public File getFileInStudyAssetsDir(String dirName, String filePath) throws IOException {
         if (filePath == null || filePath.trim().isEmpty()) {
             throw new IOException(MessagesStrings.FILE_MISSING);
         }
@@ -149,12 +140,10 @@ public class IOUtils {
     }
 
     /**
-     * Generates a filename from a name in a specified length and adds the
-     * suffix.
+     * Generates a filename from a name in a specified length and adds the suffix.
      */
     public String generateFileName(String rawName, String suffix) {
-        String filename = rawName.trim()
-                .replaceAll(REGEX_ILLEGAL_IN_FILENAME, "_").toLowerCase();
+        String filename = rawName.trim().replaceAll(REGEX_ILLEGAL_IN_FILENAME, "_").toLowerCase();
         filename = StringUtils.left(filename, MAX_FILENAME_LENGTH);
         if (suffix != null) {
             filename = filename.concat("." + suffix);
@@ -185,8 +174,7 @@ public class IOUtils {
             return;
         }
         if (!dir.isDirectory()) {
-            throw new IOException(
-                    MessagesStrings.dirPathIsntDir(dir.getName()));
+            throw new IOException(MessagesStrings.dirPathIsntDir(dir.getName()));
         }
         FileUtils.deleteDirectory(dir);
     }
@@ -195,49 +183,39 @@ public class IOUtils {
      * Copies a component's HTML file.
      *
      * @param studyAssetsDirName Name of the study assets
-     * @param htmlFilePath       Local file path to the HTML file. The file can be in a
-     *                           sub-directory of the study assets directory.
+     * @param htmlFilePath       Local file path to the HTML file. The file can be in a sub-directory of the study
+     *                           assets directory.
      * @return Name of the new file.
      */
-    public synchronized String cloneComponentHtmlFile(String studyAssetsDirName,
-            String htmlFilePath) throws IOException {
-        File htmlFile = getFileInStudyAssetsDir(studyAssetsDirName,
-                htmlFilePath);
+    public synchronized String cloneComponentHtmlFile(String studyAssetsDirName, String htmlFilePath)
+            throws IOException {
+        File htmlFile = getFileInStudyAssetsDir(studyAssetsDirName, htmlFilePath);
         if (!htmlFile.isFile()) {
-            throw new IOException(
-                    MessagesStrings.filePathIsntFile(htmlFile.getName()));
+            throw new IOException(MessagesStrings.filePathIsntFile(htmlFile.getName()));
         }
 
         File clonedHtmlFile = generateCloneFile(htmlFile);
         FileUtils.copyFile(htmlFile, clonedHtmlFile);
-        return generateLocalFilePathInStudyAssets(clonedHtmlFile,
-                studyAssetsDirName);
+        return generateLocalFilePathInStudyAssets(clonedHtmlFile, studyAssetsDirName);
     }
 
     /**
-     * Removes the part from the file's path that is the study assets path. The
-     * remaining string is only the local path within the study assets
-     * directory.
+     * Removes the part from the file's path that is the study assets path. The remaining string is only the local path
+     * within the study assets directory.
      */
-    private String generateLocalFilePathInStudyAssets(File localFile,
-            String studyAssetsDirName) {
-        String studyAssetsPath = generateStudyAssetsPath(studyAssetsDirName)
-                + File.separator;
+    private String generateLocalFilePathInStudyAssets(File localFile, String studyAssetsDirName) {
+        String studyAssetsPath = generateStudyAssetsPath(studyAssetsDirName) + File.separator;
         return localFile.getAbsolutePath().replace(studyAssetsPath, "");
     }
 
     /**
-     * Copies study assets' directory. Adds suffix '_clone' to the name of the
-     * new assets dir. If a dir with suffix '_clone' already exists it adds '_'
-     * + number instead.
+     * Copies study assets' directory. Adds suffix '_clone' to the name of the new assets dir. If a dir with suffix
+     * '_clone' already exists it adds '_' + number instead.
      */
-    public synchronized String cloneStudyAssetsDirectory(String srcDirName)
-            throws IOException {
-        File srcDir = getFileSecurely(Common.getStudyAssetsRootPath(),
-                srcDirName);
+    public synchronized String cloneStudyAssetsDirectory(String srcDirName) throws IOException {
+        File srcDir = getFileSecurely(Common.getStudyAssetsRootPath(), srcDirName);
         if (!srcDir.isDirectory()) {
-            throw new IOException(
-                    MessagesStrings.dirPathIsntDir(srcDir.getName()));
+            throw new IOException(MessagesStrings.dirPathIsntDir(srcDir.getName()));
         }
 
         File destDir = generateCloneFile(srcDir);
@@ -246,27 +224,23 @@ public class IOUtils {
     }
 
     /**
-     * Generates a filename for a clone of the given file. It tries to add the
-     * suffix '_clone'. If the file already exists in the file system it tries
-     * to add numbers starting with 1. It works with files or directories. It
-     * keeps a file extension.
+     * Generates a filename for a clone of the given file. It tries to add the suffix '_clone'. If the file already
+     * exists in the file system it tries to add numbers starting with 1. It works with files or directories. It keeps a
+     * file extension.
      */
     private File generateCloneFile(File file) throws IOException {
         String fileExtension = "";
         if (!FilenameUtils.getExtension(file.getName()).isEmpty()) {
             fileExtension = "." + FilenameUtils.getExtension(file.getName());
         }
-        String cloneFileName = FilenameUtils.removeExtension(file.getName())
-                + "_clone" + fileExtension;
+        String cloneFileName = FilenameUtils.removeExtension(file.getName()) + "_clone" + fileExtension;
 
         File parentDir = file.getParentFile();
         File clonedFile = null;
         int i = 1;
         while (clonedFile == null || clonedFile.exists()) {
-            clonedFile = getFileSecurely(parentDir.getAbsolutePath(),
-                    cloneFileName);
-            cloneFileName = FilenameUtils.removeExtension(file.getName()) + "_"
-                    + i + fileExtension;
+            clonedFile = getFileSecurely(parentDir.getAbsolutePath(), cloneFileName);
+            cloneFileName = FilenameUtils.removeExtension(file.getName()) + "_" + i + fileExtension;
             i++;
         }
         return clonedFile;
@@ -289,12 +263,9 @@ public class IOUtils {
      * @param targetDirName Name of the target dir within the assets root dir
      */
     public void moveStudyAssetsDir(File srcDir, String targetDirName) throws IOException {
-        File targetDir = getFileSecurely(Common.getStudyAssetsRootPath(),
-                targetDirName);
+        File targetDir = getFileSecurely(Common.getStudyAssetsRootPath(), targetDirName);
         if (targetDir.exists()) {
-            throw new IOException(
-                    MessagesStrings.studyAssetsDirNotCreatedBecauseExists(
-                            targetDir.getName()));
+            throw new IOException(MessagesStrings.studyAssetsDirNotCreatedBecauseExists(targetDir.getName()));
         }
         FileUtils.moveDirectory(srcDir, targetDir);
     }
@@ -316,13 +287,10 @@ public class IOUtils {
     }
 
     /**
-     * Returns all files within this directory that have the prefix and the
-     * suffix.
+     * Returns all files within this directory that have the prefix and the suffix.
      */
-    public File[] findFiles(File dir, final String prefix,
-            final String suffix) {
-        return dir.listFiles((file, name) -> name.startsWith(prefix)
-                && name.endsWith(suffix));
+    public File[] findFiles(File dir, final String prefix, final String suffix) {
+        return dir.listFiles((file, name) -> name.startsWith(prefix) && name.endsWith(suffix));
     }
 
     /**
@@ -335,8 +303,7 @@ public class IOUtils {
     /**
      * Renames a study assets dir.
      */
-    public void renameStudyAssetsDir(String oldDirName, String newDirName)
-            throws IOException {
+    public void renameStudyAssetsDir(String oldDirName, String newDirName) throws IOException {
         File oldDir = new File(generateStudyAssetsPath(oldDirName));
         File newDir = new File(generateStudyAssetsPath(newDirName));
         if (oldDir.exists() && oldDirName.equals(newDirName)) {
@@ -344,8 +311,7 @@ public class IOUtils {
         }
         if (newDir.exists()) {
             throw new IOException(
-                    MessagesStrings.studyAssetsNotRenamedBecauseExists(
-                            oldDir.getName(), newDir.getName()));
+                    MessagesStrings.studyAssetsNotRenamedBecauseExists(oldDir.getName(), newDir.getName()));
         }
         if (!oldDir.exists()) {
             createStudyAssetsDir(newDirName);
@@ -353,25 +319,21 @@ public class IOUtils {
         }
         boolean result = oldDir.renameTo(newDir);
         if (!result) {
-            throw new IOException(MessagesStrings.studyAssetsDirNotRenamed(
-                    oldDir.getName(), newDir.getName()));
+            throw new IOException(MessagesStrings.studyAssetsDirNotRenamed(oldDir.getName(), newDir.getName()));
         }
     }
 
     /**
-     * Renames a component's HTML file. This file can be in a sub-directory of
-     * the study assets directory.
+     * Renames a component's HTML file. This file can be in a sub-directory of the study assets directory.
      *
      * @param oldHtmlFilePath The current local file path within the study assets
      * @param newHtmlFilePath The new local file path within the study assets
      * @param studyAssetName  The name (not the path) of the study assets
      */
-    public void renameHtmlFile(String oldHtmlFilePath, String newHtmlFilePath,
-            String studyAssetName) throws IOException {
-        File oldHtmlFile = getFileInStudyAssetsDir(studyAssetName,
-                oldHtmlFilePath);
-        File newHtmlFile = getFileInStudyAssetsDir(studyAssetName,
-                newHtmlFilePath);
+    public void renameHtmlFile(String oldHtmlFilePath, String newHtmlFilePath, String studyAssetName)
+            throws IOException {
+        File oldHtmlFile = getFileInStudyAssetsDir(studyAssetName, oldHtmlFilePath);
+        File newHtmlFile = getFileInStudyAssetsDir(studyAssetName, newHtmlFilePath);
 
         // If the current HTML file doesn't exist or old and new are equal do
         // nothing
@@ -380,21 +342,18 @@ public class IOUtils {
         }
 
         if (newHtmlFile.exists()) {
-            throw new IOException(
-                    MessagesStrings.htmlFileNotRenamedBecauseExists(
-                            oldHtmlFilePath, newHtmlFilePath));
+            throw new IOException(MessagesStrings.htmlFileNotRenamedBecauseExists(oldHtmlFilePath, newHtmlFilePath));
         }
 
         boolean result = oldHtmlFile.renameTo(newHtmlFile);
         if (!result) {
-            throw new IOException(MessagesStrings
-                    .htmlFileNotRenamed(oldHtmlFilePath, newHtmlFilePath));
+            throw new IOException(MessagesStrings.htmlFileNotRenamed(oldHtmlFilePath, newHtmlFilePath));
         }
     }
 
     /**
-     * Creates the given File as a directory, including necessary and non-existent parent directories.
-     * If the file already exists it will be deleted before.
+     * Creates the given File as a directory, including necessary and non-existent parent directories. If the file
+     * already exists it will be deleted before.
      */
     public static void createDir(File file) throws IOException {
         if (!file.mkdirs()) {
