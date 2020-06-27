735a4200d47139a28c1ccca2dde1fc0e143a0c6b
kristian <kristian.lange@talentwunder.com>
2020-06-27 23:43:16 +0200

    MTWorkerDao.java - fix case-insensitive comparision

--- modules/common/app/daos/common/worker/MTWorkerDao.java
+++ modules/common/app/daos/common/worker/MTWorkerDao.java
@@ -41,13 +41,12 @@ public class MTWorkerDao extends WorkerDao {
     }
 
     /**
-     * Retrieves the worker with the given MTurk worker ID in a case insensitive
-     * way.
+     * Retrieves the worker with the given MTurk worker ID in a case insensitive way.
      */
     public Optional<MTWorker> findByMTWorkerId(String mtWorkerId) {
         String queryStr = "SELECT w FROM Worker w WHERE upper(w.mtWorkerId)=:mtWorkerId";
         TypedQuery<Worker> query = jpa.em().createQuery(queryStr, Worker.class);
-        query.setParameter("mtWorkerId", mtWorkerId);
+        query.setParameter("mtWorkerId", mtWorkerId.toUpperCase());
         query.setMaxResults(1);
         List<Worker> workerList = query.getResultList();
         return !workerList.isEmpty() ? Optional.of((MTWorker) workerList.get(0)) : Optional.empty();
