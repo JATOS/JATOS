e45e6c6c9325b5506950a5ed6cc5ebde12b2b029
kristian <kristian.lange@talentwunder.com>
2020-08-23 22:53:33 +0200

    version to 3.5.6

--- modules/publix/app/assets/javascripts/jatos.js
+++ modules/publix/app/assets/javascripts/jatos.js
@@ -30,7 +30,7 @@ var jatos = {};
 	/**
 	 * jatos.js version
 	 */
-	jatos.version = "3.5.6-alpha";
+	jatos.version = "3.5.6";
 	/**
 	 * How long in ms should JATOS wait before retrying the HTTP call.
 	 */
