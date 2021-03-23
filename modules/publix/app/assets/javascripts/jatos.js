07c6275e19752919c9a6488d61fbb332c0ad3472
kristian <kristian.lange@talentwunder.com>
2021-03-23 21:39:00 +0100

    version to 3.5.11

--- modules/publix/app/assets/javascripts/jatos.js
+++ modules/publix/app/assets/javascripts/jatos.js
@@ -30,7 +30,7 @@ var jatos = {};
 	/**
 	 * jatos.js version
 	 */
-	jatos.version = "3.5.10";
+	jatos.version = "3.5.11";
 	/**
 	 * How long in ms should JATOS wait before retrying the HTTP call.
 	 */
