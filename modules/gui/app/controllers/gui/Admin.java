c8bd5616961fed18c8d67659d37e93ab0543e2b9
kristian <kristian.lange@talentwunder.com>
2021-05-21 18:32:48 +0200

    admin page - allow downloading of all log files in logs folder

--- modules/gui/app/controllers/gui/Admin.java
+++ modules/gui/app/controllers/gui/Admin.java
@@ -1,16 +1,23 @@
 package controllers.gui;
 
+import akka.stream.javadsl.FileIO;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
 import controllers.gui.actionannotations.AuthenticationAction.Authenticated;
 import controllers.gui.actionannotations.GuiAccessLoggingAction.GuiAccessLogging;
 import daos.common.StudyDao;
 import daos.common.StudyResultDao;
 import daos.common.UserDao;
 import daos.common.worker.WorkerDao;
+import general.common.Common;
 import models.common.User;
 import models.common.User.Role;
+import play.core.utils.HttpHeaderParameterEncoding;
 import play.db.jpa.Transactional;
+import play.http.HttpEntity;
 import play.mvc.Controller;
 import play.mvc.Http;
+import play.mvc.ResponseHeader;
 import play.mvc.Result;
 import services.gui.AdminService;
 import services.gui.AuthenticationService;
@@ -21,9 +28,13 @@ import utils.common.JsonUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Controller class around administration (updates are handled in Updates and user manager in Users)
@@ -69,34 +80,61 @@ public class Admin extends Controller {
     }
 
     /**
-     * Returns the content of a log file in reverse order and as 'Transfer-Encoding:chunked'. It does so only if an
-     * user with Role ADMIN is logged in. It limits the number of lines to the given lineLimit. If the log file can't
-     * be read it still returns with OK but instead of the file content with an error message.
+     * Returns the content (all regular file's names) of the logs directory as JSON
      */
     @Transactional
     @Authenticated(Role.ADMIN)
-    public Result log(String filename, Integer lineLimit) {
-        if (!filename.matches("^application.log|update.log|loader.log$")) return badRequest();
-        return ok().chunked(logFileReader.read(filename, lineLimit)).as("text/plain; charset=utf-8");
+    public Result listLogs() throws IOException {
+        try (Stream<Path> paths = Files.walk(Paths.get(Common.getBasepath() + "/logs/"))) {
+            List<String> content = paths
+                    .filter(Files::isRegularFile)
+                    .map(file -> file.getFileName().toString())
+                    .sorted()
+                    .collect(Collectors.toList());
+            return ok(JsonUtils.asJson(content));
+        }
     }
 
     /**
-     * Ajax request
-     *
+     * Returns the log file specified by 'filename'. If 'reverse' is true, it returns the content of the file in
+     * reverse order and as 'Transfer-Encoding:chunked'. It limits the number of lines to the given lineLimit. If the
+     * log file can't be read it still returns with OK but instead of the file content with an error message.
+     * If 'reverse' is false it returns the file for download.
+     */
+    @Transactional
+    @Authenticated(Role.ADMIN)
+    public Result logs(String filename, Integer lineLimit, boolean reverse) {
+        if (reverse) {
+            return ok().chunked(logFileReader.read(filename, lineLimit)).as("text/plain; charset=utf-8");
+        } else {
+            Path logPath = Paths.get(Common.getBasepath() + "/logs/" + filename);
+            if (Files.notExists(logPath)) {
+                return notFound();
+            }
+            Source<ByteString, ?> source = FileIO.fromPath(logPath);
+            Optional<Long> contentLength = Optional.of(logPath.toFile().length());
+            String filenameInHeader = HttpHeaderParameterEncoding.encode("filename", filename);
+            return new Result(new ResponseHeader(200, Collections.emptyMap()),
+                    new HttpEntity.Streamed(source, contentLength, Optional.empty()))
+                    .withHeader(Http.HeaderNames.CONTENT_DISPOSITION, "attachment; " + filenameInHeader);
+        }
+    }
+
+    /**
      * Returns some status values
      */
     @Transactional
     @Authenticated(Role.ADMIN)
     public Result status() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("studyCount", studyDao.count());
-        map.put("studyResultCount", studyResultDao.count());
-        map.put("workerCount", workerDao.count());
-        map.put("userCount", userDao.count());
-        map.put("serverTime", Helpers.formatTimestamp(new Date()));
-        map.put("latestUsers", adminService.getLatestUsers(10));
-        map.put("latestStudyRuns", adminService.getLatestStudyRuns(10));
-        return ok(JsonUtils.asJson(map));
+        Map<String, Object> statusMap = new HashMap<>();
+        statusMap.put("studyCount", studyDao.count());
+        statusMap.put("studyResultCount", studyResultDao.count());
+        statusMap.put("workerCount", workerDao.count());
+        statusMap.put("userCount", userDao.count());
+        statusMap.put("serverTime", Helpers.formatTimestamp(new Date()));
+        statusMap.put("latestUsers", adminService.getLatestUsers(10));
+        statusMap.put("latestStudyRuns", adminService.getLatestStudyRuns(10));
+        return ok(JsonUtils.asJson(statusMap));
     }
 
     /**
@@ -111,8 +149,6 @@ public class Admin extends Controller {
     }
 
     /**
-     * Ajax request
-     *
      * Returns table data for study admin page
      */
     @Transactional
@@ -122,8 +158,6 @@ public class Admin extends Controller {
     }
 
     /**
-     * Ajax request
-     *
      * Returns admin data for all studies that belong to the given user
      */
     @Transactional
