f75a04dd2a54efe28f366744a64fe20239026efa
kristian <kristian.lange@talentwunder.com>
2021-07-01 22:31:12 +0200

    PublixHelpers.java - clean up unused methods

--- modules/publix/app/services/publix/PublixHelpers.java
+++ modules/publix/app/services/publix/PublixHelpers.java
@@ -7,71 +7,56 @@ import models.common.StudyResult;
 import models.common.StudyResult.StudyState;
 import models.common.workers.Worker;
 
-import java.util.List;
-
 /**
  * @author Kristian Lange
  */
-public abstract class PublixHelpers {
+public class PublixHelpers {
 
-	/**
-	 * Checks if the worker finished this study already at least once. 'Finished' includes failed and aborted.
-	 */
-	public static boolean finishedStudyAlready(Worker worker, Study study) {
-		for (StudyResult studyResult : worker.getStudyResultList()) {
-			if (studyResult.getStudy().equals(study) && studyDone(studyResult)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Checks if the worker finished this study already at least once. 'Finished' includes failed and aborted.
+     */
+    public static boolean finishedStudyAlready(Worker worker, Study study) {
+        for (StudyResult studyResult : worker.getStudyResultList()) {
+            if (studyResult.getStudy().equals(study) && studyDone(studyResult)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Checks if the worker ever did this study (independent of the study
-	 * result's state).
-	 */
-	public static boolean didStudyAlready(Worker worker, Study study) {
-		for (StudyResult studyResult : worker.getStudyResultList()) {
-			if (studyResult.getStudy().equals(study)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Checks if the worker ever did this study (independent of the study
+     * result's state).
+     */
+    public static boolean didStudyAlready(Worker worker, Study study) {
+        for (StudyResult studyResult : worker.getStudyResultList()) {
+            if (studyResult.getStudy().equals(study)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * True if StudyResult's state is in FINISHED or ABORTED or FAIL. False
-	 * otherwise.
-	 */
-	public static boolean studyDone(StudyResult studyResult) {
-		StudyState state = studyResult.getStudyState();
-		return state == StudyState.FINISHED || state == StudyState.ABORTED
-				|| state == StudyState.FAIL;
-	}
+    /**
+     * True if StudyResult's state is in FINISHED or ABORTED or FAIL. False
+     * otherwise.
+     */
+    public static boolean studyDone(StudyResult studyResult) {
+        StudyState state = studyResult.getStudyState();
+        return state == StudyState.FINISHED || state == StudyState.ABORTED
+                || state == StudyState.FAIL;
+    }
 
-	/**
-	 * Returns true only if every single StudyResult's state from the given list
-	 * is in FINISHED or ABORTED or FAIL. False otherwise.
-	 */
-	public boolean allStudiesDone(List<StudyResult> studyResultList) {
-		for (StudyResult studyResult : studyResultList) {
-			if (!PublixHelpers.studyDone(studyResult)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * True if ComponentResult's state is in FINISHED or ABORTED or FAIL or
-	 * RELOADED. False otherwise.
-	 */
-	public static boolean componentDone(ComponentResult componentResult) {
-		ComponentState state = componentResult.getComponentState();
-		return ComponentState.FINISHED == state
-				|| ComponentState.ABORTED == state
-				|| ComponentState.FAIL == state
-				|| ComponentState.RELOADED == state;
-	}
+    /**
+     * True if ComponentResult's state is in FINISHED or ABORTED or FAIL or
+     * RELOADED. False otherwise.
+     */
+    public static boolean componentDone(ComponentResult componentResult) {
+        ComponentState state = componentResult.getComponentState();
+        return ComponentState.FINISHED == state
+                || ComponentState.ABORTED == state
+                || ComponentState.FAIL == state
+                || ComponentState.RELOADED == state;
+    }
 
 }
