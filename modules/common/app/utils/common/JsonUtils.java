0f9b557aecd1207623c733b7d5571e651aaf4ffa
Kristian Lange <lange.kristian@gmail.com>
2021-10-05 14:27:25 +0200

    jatos.urlQueryParameters - fix JSON deserialization

--- modules/common/app/utils/common/JsonUtils.java
+++ modules/common/app/utils/common/JsonUtils.java
@@ -131,7 +131,7 @@ public class JsonUtils {
         initData.set("batchProperties", Json.mapper().readTree(batchProperties));
         initData.set("componentList", componentList);
         initData.set("componentProperties", Json.mapper().readTree(componentProperties));
-        initData.put("urlQueryParameters", urlQueryParameters);
+        initData.put("urlQueryParameters", Json.mapper().readTree(urlQueryParameters));
         initData.put("studyLinkId", studyLinkId);
         return initData;
     }
