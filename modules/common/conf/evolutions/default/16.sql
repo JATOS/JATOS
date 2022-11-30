30f163c63d807a740c157f42cee1cd55ec43c04e
kristian <lange.kristian@gmail.com>
2022-11-30 12:57:07 +0100

    16.sql - Add indices for StudyResult.uuid and Batch.uuid

--- modules/common/conf/evolutions/default/16.sql
+++ modules/common/conf/evolutions/default/16.sql
@@ -1,9 +1,11 @@
-# --- Add dataShort and dataSize to ComponentResult table
+# --- Add dataShort and dataSize to ComponentResult table; Add indices to uuid of StudyResult, Batch and Component
 
 # --- !Ups
 ALTER TABLE `ComponentResult` ADD `dataShort` varchar(1023) DEFAULT NULL;
 ALTER TABLE `ComponentResult` ADD `dataSize` bigint(20) DEFAULT NULL;
 
+ALTER TABLE `StudyResult` ADD UNIQUE KEY `FKzl0vqfy8qoopgrkkcp1q5qja` (`uuid`);
+ALTER TABLE `Batch` ADD UNIQUE KEY `FKlskzulbu7gz7mktw9zv6dso4` (`uuid`);
 
 # --- !Downs
 # --- not supported
