f365d7c25a631872f0684fe3d0f8a0be48cbf152
kristian <lange.kristian@gmail.com>
2022-12-06 17:12:16 +0100

    docker_buildx.sh - add -t latest

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -3,3 +3,5 @@
 sbt docker:publishLocal
 
 docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.5 target/docker/stage
+
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.5 -d jatos/jatos:latest target/docker/stage
