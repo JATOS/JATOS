84ec9f976b9b3a407dcde8234f24206e964fc0f2
kristian <lange.kristian@gmail.com>
2023-01-14 22:47:14 +0100

    Bump version to 3.7.6

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -2,6 +2,6 @@
 
 sbt docker:publishLocal
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.5 target/docker/stage
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.6 target/docker/stage
 
-#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.5 -d jatos/jatos:latest target/docker/stage
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.6 -d jatos/jatos:latest target/docker/stage
--- modules/publix/public/javascripts/jatos.js
+++ modules/publix/public/javascripts/jatos.js
@@ -32,7 +32,7 @@ var jatos = {};
 	/**
 	 * jatos.js version
 	 */
-	jatos.version = "3.7.5";
+	jatos.version = "3.7.6";
 	/**
 	 * How long in ms should JATOS wait before retrying the HTTP call.
 	 */
