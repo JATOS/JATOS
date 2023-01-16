9a52664e144d52213a86e549bf021fa36bae6136
kristian <lange.kristian@gmail.com>
2023-01-16 17:05:42 +0100

    docker_buildx.sh - change -d to -t

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -4,4 +4,4 @@ sbt docker:publishLocal
 
 docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.6 target/docker/stage
 
-#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.6 -d jatos/jatos:latest target/docker/stage
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.7.6 -t jatos/jatos:latest target/docker/stage
