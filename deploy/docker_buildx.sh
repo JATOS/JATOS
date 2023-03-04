931d841edb55aa5fe04289336c543113736cbcb0
kristian <lange.kristian@gmail.com>
2023-03-04 17:44:01 +0100

    Change version to 3.8.1

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -2,6 +2,6 @@
 
 sbt docker:publishLocal
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.8.1-alpha target/docker/stage
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.8.1 target/docker/stage
 
-#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.8.1-alpha -t jatos/jatos:latest target/docker/stage
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.8.1 -t jatos/jatos:latest target/docker/stage
