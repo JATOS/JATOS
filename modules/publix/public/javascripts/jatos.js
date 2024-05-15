a226e799c4e57ddfadfae04970e8b8960d72a65e
kristian <lange.kristian@gmail.com>
2024-05-15 22:22:29 +0200

    Increase jQuery version to 3.7.1 in jatos.js

--- modules/publix/public/javascripts/jatos.js
+++ modules/publix/public/javascripts/jatos.js
@@ -270,7 +270,7 @@ var jatos = {};
 	// Load jatos.js's jQuery and put it in jatos.jQuery to avoid conflicts with
 	// a component's jQuery version. Afterwards call initJatos.
 	jatos.jQuery = {};
-	getScript('jatos-publix/javascripts/jquery-3.5.1.min.js', function () {
+	getScript('jatos-publix/javascripts/jquery-3.7.1.min.js', function () {
 		jatos.jQuery = jQuery.noConflict(true);
 		jatos.jQuery.ajaxSetup({
 			cache: true
