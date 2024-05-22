c555b10fe1da7588df1304dc372a5219a869ca01
kristian <lange.kristian@gmail.com>
2024-05-22 22:38:25 +0200

    GUI / waitingModal.js - fix Cancel button

--- modules/gui/app/assets/javascripts/waitingModal.js
+++ modules/gui/app/assets/javascripts/waitingModal.js
@@ -3,19 +3,30 @@ export { show, hide }
 import * as Helpers from "./helpers.js";
 import * as FileSystemAccess from "./fileSystemAccess.js";
 
+// The waiting modal might be shown multiple times, so we have an array with all timeouts and when we hide the modal we
+// clear them all.
 const timeouts = [];
 
-function show(cancelable, msg) {
+/*
+ * Shows the waiting modal after 2 secs of waiting
+ *
+ * @param {boolean} cancelable - Show the cancel button. So far the cancel button only aborts file downloads done by
+ *                               fileSystemAccess.js
+ */
+function show(cancelable = false) {
     if (typeof cancelable === 'boolean' && cancelable === true) {
-        $('#waitingModal .modal-footer').show();
+        $('#waitingModalCancel').show();
     } else {
-        $('#waitingModal .modal-footer').hide();
+        $('#waitingModalCancel').hide();
     }
-    hide(msg);
+    hide(); // First hide an 'old' waiting modals
     timeouts.push(setTimeout(function() { $('#waitingModal').modal('show') }, 2000));
 }
 
-function hide(msg) {
+/*
+ * Hides the waiting modal and clear all timeouts
+ */
+function hide() {
     for (let i = 0; i < timeouts.length; i++) {
         clearTimeout(timeouts[i]);
     }
