7d4430a5694c6bc01d6eaa63714a654706cf52bc
kristian <lange.kristian@gmail.com>
2024-05-24 23:26:44 +0200

    GUI / qrCodeModal - use JATOS' standard Modal subtitles; add some docs

--- modules/gui/app/assets/javascripts/qrCodeModal.js
+++ modules/gui/app/assets/javascripts/qrCodeModal.js
@@ -1,15 +1,26 @@
+/*
+ * Shows QR codes in a Modal. Uses easy.qrcode.js included in main.scala.html.
+ * The Modal is defined in qrCodeModal.scala.html.
+ */
+
 export { show }
 
-$(".btn-qr-code").on("click", show);
+import * as Helpers from "./helpers.js";
 
-// Use this function to add an event handler manually
+// Add a click event listener to every ".btn-qr-code". But we need the ".off" in case this script is
+// imported multiple times on the same page
+$(".btn-qr-code").off("click").on("click", show);
+
+/*
+ * Use this function to add an event listener manually, e.g. for dynamically generated elements
+ */
 function show() {
     const button = this;
     const url = button.parentElement.textContent.trim();
 
     $('#qrcode').empty();
     $('#qrCodeModal').off('shown.bs.modal');
-    $('#qrCodeModalStudyLink').html(url);
+    Helpers.generateModalSubtitles("#qrCodeModal", {"Study link": url});
 
     $('#qrCodeModal').on('shown.bs.modal', function (e) {
         let options = {
