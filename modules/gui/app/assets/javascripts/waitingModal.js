e5c12bae6a751e4da8c86eabea9b1706d514f43a
kristian <lange.kristian@gmail.com>
2024-06-18 21:50:10 +0200

    waitingModal.js - hide modals that are in the process of being shown

--- modules/gui/app/assets/javascripts/waitingModal.js
+++ modules/gui/app/assets/javascripts/waitingModal.js
@@ -1,11 +1,10 @@
 export { show, hide }
 
-import * as Helpers from "./helpers.js";
 import * as FileSystemAccess from "./fileSystemAccess.js";
 
 // The waiting modal might be shown multiple times, so we have an array with all timeouts and when we hide the modal we
-// clear them all.
-const timeouts = [];
+// clear them all. We are using timeouts because Bootstrap's 'data-bs-delay' attribute didn't seem to work.
+let timeouts = [];
 
 /*
  * Shows the waiting modal after 2 secs of waiting
@@ -19,7 +18,12 @@ function show(cancelable = false) {
     } else {
         $('#waitingModalCancel').hide();
     }
-    hide(); // First hide an 'old' waiting modals
+    hide(); // First hide an 'old' waiting modal
+
+    // We have to remove any event handler originating in the hide function
+    $('#waitingModal').off('shown.bs.modal');
+
+    // I couldn't get 'data-bs-delay' working and therefore I'm using 'setTimeout'
     timeouts.push(setTimeout(function() { $('#waitingModal').modal('show') }, 2000));
 }
 
@@ -30,7 +34,11 @@ function hide() {
     for (let i = 0; i < timeouts.length; i++) {
         clearTimeout(timeouts[i]);
     }
+    timeouts = [];
+
     $('#waitingModal').modal('hide');
+    // We also have to deal with the Modals that are in the process of being shown
+    $('#waitingModal').one('shown.bs.modal', () => $('#waitingModal').modal('hide'));
 }
 
 $('#waitingModalCancel').click(() => {
