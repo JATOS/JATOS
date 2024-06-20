9a063d48fa87a294b7d06a6bf2bfe401b6c080e9
Kristian <k.lange@iwm-tuebingen.de>
2024-06-20 23:51:54 +0200

    GUI / tooltips - double time for tooltips to appear (2s instead of 1s)

--- modules/gui/app/assets/javascripts/helpers.js
+++ modules/gui/app/assets/javascripts/helpers.js
@@ -306,7 +306,7 @@ function activateTooltips(parent) {
         $(this).attr('data-bs-title', title);
 
         // Add our default attributes if they aren't already set
-        if (!$(this).is('[data-bs-delay]')) $(this).attr('data-bs-delay', '{"show":1000, "hide":150}');
+        if (!$(this).is('[data-bs-delay]')) $(this).attr('data-bs-delay', '{"show":2000, "hide":150}');
         if (!$(this).is('[data-bs-trigger]')) $(this).attr('data-bs-trigger', 'hover');
         if (!$(this).is('[data-bs-container]')) $(this).attr('data-bs-container', 'body');
 
