98c1be4241cb84bcf46812e0501722ef52405786
Kristian <k.lange@iwm-tuebingen.de>
2024-06-21 23:08:50 +0200

    Increase version to 3.9.2

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -2,6 +2,6 @@
 
 sbt docker:publishLocal
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.1-alpha target/docker/stage
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.2 target/docker/stage
 
-#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.1 -t jatos/jatos:latest target/docker/stage
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.2 -t jatos/jatos:latest target/docker/stage
