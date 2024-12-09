3bac00259aa19e78dae75fced4854ceec66e0e0c
kristian <lange.kristian@gmail.com>
2024-12-09 22:13:50 +0100

    Increase version to 3.9.5

--- modules/gui/build.sbt
+++ modules/gui/build.sbt
@@ -1,5 +1,5 @@
 name := "jatos-gui"
-version := "3.9.4"
+version := "3.9.5"
 organization := "org.jatos"
 scalaVersion := "2.13.8"
 maintainer := "lange.kristian@gmail.com"
--- modules/publix/public/javascripts/jatos.js
+++ modules/publix/public/javascripts/jatos.js
@@ -31,7 +31,7 @@ var jatos = {};
 	/**
 	 * jatos.js version
 	 */
-	jatos.version = "3.9.4";
+	jatos.version = "3.9.5";
 	/**
 	 * How long in ms should JATOS wait before retrying the HTTP call.
 	 */
