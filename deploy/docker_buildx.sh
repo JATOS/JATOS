5a2f6a8e6729df8da2e6acd90d99342bd482e2e1
Kristian Lange <lange.kristian@gmail.com>
2025-05-08 20:45:56 +0200

    Increase version to 3.9.6

--- deploy/docker_buildx.sh
+++ deploy/docker_buildx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#sbt docker:publishLocal
+sbt docker:publishLocal
 
-#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.4 target/docker/stage
+#docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.6 target/docker/stage
 
-docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.4 -t jatos/jatos:latest target/docker/stage
+docker buildx build --platform=linux/arm64,linux/amd64 --push -t jatos/jatos:3.9.6 -t jatos/jatos:latest target/docker/stage
