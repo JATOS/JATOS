aeceb7db88529b4f6933d9d6f1773e7e279bc5ad
Kristian Lange <lange.kristian@gmail.com>
2025-11-27 22:16:14 +0100

    Update OpenAIProxy to use Common.getOpenAIApiKey and handle missing API key with 401 HTTP status

--- modules/publix/app/controllers/publix/OpenAIProxy.java
+++ modules/publix/app/controllers/publix/OpenAIProxy.java
@@ -1,30 +1,34 @@
 package controllers.publix;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Strings;
+import general.common.Common;
 import play.libs.ws.WSClient;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 public class OpenAIProxy extends Controller {
 
     private final WSClient ws;
-    private final String openaiKey;
 
     @Inject
     public OpenAIProxy(WSClient ws) {
         this.ws = ws;
-        this.openaiKey = System.getenv("OPENAI_API_KEY");
     }
 
     public CompletionStage<Result> proxy(Http.Request request, String path) {
-        JsonNode body = request.body().asJson();
+        if (Strings.isNullOrEmpty(Common.getOpenAIApiKey())) {
+            return CompletableFuture.completedFuture(status(401));
+        }
 
+        JsonNode body = request.body().asJson();
         return ws.url("https://api.openai.com/v1/" + path)
-                .addHeader("Authorization", "Bearer " + openaiKey)
+                .addHeader("Authorization", "Bearer " + Common.getOpenAIApiKey())
                 .post(body)
                 .thenApply(r -> status(r.getStatus(), r.getBody()));
     }
