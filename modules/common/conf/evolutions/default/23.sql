458c4e0f67679465f015a5bdac5cf4917852f1f0
Kristian Lange <lange.kristian@gmail.com>
2026-03-29 00:55:00 +0100

    Update SQL migration: add `id` column to `User` table with unique constraint and populate values in a H2 and MySQL compatible way

--- modules/common/conf/evolutions/default/23.sql
+++ modules/common/conf/evolutions/default/23.sql
@@ -2,11 +2,24 @@
 # --- Add row openAiApiCount to StudyResult table
 
 # --- !Ups
-
-ALTER TABLE `User` ADD COLUMN `id` BIGINT(29) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `User` ADD UNIQUE KEY `UK_m9r2kq7t1v8n3c6p0a4x5j2d` (`id`);
-
 ALTER TABLE `StudyResult` ADD `openAiApiCount` INT NOT NULL DEFAULT 0;
+
+ALTER TABLE `User` ADD COLUMN `id` BIGINT NULL;
+
+CREATE TEMPORARY TABLE `user_id_map` (
+    `username` VARCHAR(255) NOT NULL,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO `user_id_map` (`username`) SELECT `username` FROM `User` ORDER BY `username`;
+
+UPDATE `User` u SET `id` = (
+    SELECT m.`id` FROM `user_id_map` m WHERE m.`username` = u.`username`
+);
+
+ALTER TABLE `User` ADD UNIQUE KEY `UK_m9r2kq7t1v8n3c6p0a4x5j2d` (`id`);
+ALTER TABLE `User` MODIFY COLUMN `id` BIGINT AUTO_INCREMENT;
 
 # --- !Downs
 # --- not supported
