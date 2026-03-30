51b69e0c622469009401754b61e0d64232526515
Kristian Lange <lange.kristian@gmail.com>
2026-03-30 16:32:42 +0200

    GUI - Rename OpenApiController to OpenApi and update route/controller logic for serving OpenAPI specs

--- modules/gui/app/controllers/gui/OpenApi.java
+++ modules/gui/app/controllers/gui/OpenApi.java
@@ -1,17 +1,19 @@
 package controllers.gui;
 
+import general.common.Common;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Singleton;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Singleton
-public class OpenApiController extends Controller {
+public class OpenApi extends Controller {
 
-    public Result openapi() {
-        var path = Paths.get("jatos-api.yaml");
+    public Result specs() {
+        var path = Paths.get(Common.getBasepath() + File.separator + "jatos-api.yaml");
         if (Files.exists(path)) {
             try {
                 return ok(Files.readString(path)).as("text/yaml");
