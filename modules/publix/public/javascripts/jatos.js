d0e36d3c37205262de4e7a975cc0880da7e26b45
Kristian Lange <lange.kristian@gmail.com>
2026-05-12 22:59:10 +0200

    Bump version to 3.10.3 across all modules and files

--- modules/publix/public/javascripts/jatos.js
+++ modules/publix/public/javascripts/jatos.js
@@ -32,7 +32,7 @@ window.jatos = jatos; // Make jatos available in the window object for backward 
     /**
      * jatos.js version
      */
-    jatos.version = "3.10.2";
+    jatos.version = "3.10.3";
     /**
      * How long in ms should JATOS wait before retrying the HTTP call.
      */
