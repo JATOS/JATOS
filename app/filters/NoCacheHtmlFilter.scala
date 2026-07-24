768354acd3586eb257368d3bf6c90b7541c601a7
Kristian Lange <lange.kristian@gmail.com>
2026-07-24 21:00:55 +0200

    Simplify NoCacheHtmlFilter by by using result.withHeaders

--- app/filters/NoCacheHtmlFilter.scala
+++ app/filters/NoCacheHtmlFilter.scala
@@ -19,28 +19,24 @@ class NoCacheHtmlFilter @Inject()(implicit val mat: Materializer, ec: ExecutionC
     val context = Context.current()
 
     next(request).map { result =>
-      Context.withContext(context, new Supplier[Result] {
-        override def get(): Result = {
-          val shouldApply = request.path.startsWith("/jatos")
-          val contentType = result.body.contentType.getOrElse("")
+      Context.withContext(context, () => {
+        val shouldApply = request.path.startsWith("/jatos")
+        val contentType = result.body.contentType.getOrElse("")
 
-          if (shouldApply && contentType.startsWith("text/html")) {
-            val existingHeaders = result.header.headers
-            val noCacheHeaders = Map(
-              "Cache-Control" -> "no-cache, no-store, must-revalidate",
-              "Pragma" -> "no-cache",
-              "Expires" -> "0"
-            )
+        if (shouldApply && contentType.startsWith("text/html")) {
+          val existingHeaders = result.header.headers
+          val noCacheHeaders = Map(
+            "Cache-Control" -> "no-cache, no-store, must-revalidate",
+            "Pragma" -> "no-cache",
+            "Expires" -> "0"
+          )
 
-            val headersToAdd = noCacheHeaders.filterNot { case (name, _) =>
-              existingHeaders.keys.exists(_.equalsIgnoreCase(name))
-            }
-
-            headersToAdd.foreach { case (name, value) =>
-              Context.current().response().setHeader(name, value)
-            }
+          val headersToAdd = noCacheHeaders.filterNot { case (name, _) =>
+            existingHeaders.keys.exists(_.equalsIgnoreCase(name))
           }
 
+          result.withHeaders(headersToAdd.toSeq: _*)
+        } else {
           result
         }
       })
