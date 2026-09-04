b3a2e048af09092d46ac41c770bb058915518602
Kristian Lange <lange.kristian@gmail.com>
2026-09-04 12:43:35 +0200

    `BatchDao.addWorkerToBatch` - replaced `WHERE NOT EXISTS` logic with `INSERT IGNORE` for compatibility with MariaDB.

--- modules/common/app/daos/common/BatchDao.java
+++ modules/common/app/daos/common/BatchDao.java
@@ -93,13 +93,8 @@ public class BatchDao extends AbstractDao {
     }
 
     public void addWorkerToBatch(Long batchId, Long workerId) {
-        jpa.em().createNativeQuery(
-                        "INSERT INTO BatchWorkerMap (batch_id, worker_id) "
-                                + "SELECT :batchId, :workerId "
-                                + "WHERE NOT EXISTS ( "
-                                + "  SELECT 1 FROM BatchWorkerMap "
-                                + "  WHERE batch_id = :batchId AND worker_id = :workerId"
-                                + ")")
+        jpa.em().createNativeQuery("INSERT IGNORE INTO BatchWorkerMap (batch_id, worker_id) "
+                        + "VALUES (:batchId, :workerId)")
                 .setParameter("batchId", batchId)
                 .setParameter("workerId", workerId)
                 .executeUpdate();
