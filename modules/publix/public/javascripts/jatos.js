83f91e866230e352d7a7a467f613236a2f139a7d
Kristian Lange <lange.kristian@gmail.com>
2026-09-19 21:06:53 +0200

    Bump version to 3.11.3 across all modules and update Docker image tag

--- modules/publix/public/javascripts/jatos.js
+++ modules/publix/public/javascripts/jatos.js
@@ -27,7 +27,7 @@ window.jatos = jatos; // Make jatos available in the window object for backward 
     /**
      * jatos.js version
      */
-    jatos.version = "3.11.2";
+    jatos.version = "3.11.3";
     /**
      * How long in ms should JATOS wait before retrying the HTTP call.
      */
